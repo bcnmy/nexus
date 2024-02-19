d717d5d0ac51835bac5c8240969b4bf4188553b6
aboudjem <boudjemaa.adam@gmail.com>
2024-02-20 02:29:09 +0400

    🚀 Add deployment of SmartAccount contract

--- scripts/hardhat/deploy.ts
+++ scripts/hardhat/deploy.ts
@@ -1,22 +1,13 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const SmartAccount = await ethers.getContractFactory("SmartAccount");
 
-  const lockedAmount = ethers.parseEther("0.001");
+  const smartAccount = await SmartAccount.deploy();
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  await smartAccount.waitForDeployment();
 
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount,
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`,
-  );
+  console.log(`SmartAccount deployed to: ${await smartAccount.getAddress}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
