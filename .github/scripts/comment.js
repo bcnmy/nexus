6240c57f15a9f91fc387c0198ebda38514ec065b
aboudjem <boudjemaa.adam@gmail.com>
2024-02-20 21:23:29 +0400

    Add check for pull request event in comment.js

--- .github/scripts/comment.js
+++ .github/scripts/comment.js
@@ -1,6 +1,12 @@
 module.exports = async ({ github, context, header, body }) => {
     const comment = [header, body].join("\n");
   
+    // Check if the workflow is triggered by a pull request event
+    if (!context.payload.pull_request) {
+        console.log('This workflow is not triggered by a pull request. Skipping comment creation/update.');
+        return;
+    }
+  
     const { data: comments } = await github.rest.issues.listComments({
       owner: context.repo.owner,
       repo: context.repo.repo,
@@ -19,5 +25,4 @@ module.exports = async ({ github, context, header, body }) => {
       body: comment,
       ...(botComment ? { comment_id: botComment.id } : { issue_number: context.payload.pull_request.number }),
     });
-  };
-  
+};
