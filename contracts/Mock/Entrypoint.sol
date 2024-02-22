233cc360135dd77c0ccc17dcaaeb57a23c2b6b44
aboudjem <boudjemaa.adam@gmail.com>
2024-02-22 16:13:03 +0400

    Remove unused contracts and imports

--- contracts/Mock/Entrypoint.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.8.24;
-
-import { EntryPoint } from "account-abstraction/contracts/core/EntryPoint.sol";
-
-contract Entrypoint is EntryPoint {
-// solhint-disable-previous-line no-empty-blocks
-}
