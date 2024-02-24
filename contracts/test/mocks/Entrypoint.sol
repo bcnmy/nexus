e2dbf1ed7e6d3c3938387c3a0dc7f9731af5ac66
aboudjem <boudjemaa.adam@gmail.com>
2024-02-24 16:42:37 +0400

    🔥 remove empty useless contract

--- contracts/test/mocks/Entrypoint.sol
+++ contracts/test/mocks/Entrypoint.sol
@@ -2,7 +2,3 @@
 pragma solidity >=0.8.24;
 
 import { EntryPoint } from "account-abstraction/contracts/core/EntryPoint.sol";
-
-contract Entrypoint is EntryPoint {
-// solhint-disable-previous-line no-empty-blocks
-}
