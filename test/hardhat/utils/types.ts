14a53bb2e012f542bcce04c93b2c190d0afe3bc1
aboudjem <boudjemaa.adam@gmail.com>
2024-02-24 16:36:58 +0400

    🎨 improve code with interface

--- test/hardhat/utils/types.ts
+++ test/hardhat/utils/types.ts
@@ -1,26 +1,58 @@
+import { NumberLike } from "@nomicfoundation/hardhat-network-helpers/dist/src/types";
+import { AddressLike, BigNumberish, BytesLike, Signer } from "ethers";
+import {
+  AccountFactory,
+  Counter,
+  EntryPoint,
+  MockValidator,
+  SmartAccount,
+} from "../../../typechain-types";
+
+export interface DeploymentFixture {
+  entryPoint: EntryPoint;
+  smartAccount: SmartAccount;
+  factory: AccountFactory;
+  module: MockValidator;
+  counter: Counter;
+  accounts: Signer[];
+  addresses: string[];
+}
 export interface UserOperation {
-  sender: string;
-  nonce: number | string | bigint;
-  initCode?: string;
-  callData?: string;
-  callGasLimit?: number | string | bigint;
-  verificationGasLimit?: number | string | bigint;
-  preVerificationGas?: number | string | bigint;
-  maxFeePerGas?: number | string | bigint;
-  maxPriorityFeePerGas?: number | string | bigint;
-  paymaster?: string;
-  paymasterData?: string;
-  signature?: string;
+  sender: AddressLike;
+  nonce?: BigNumberish;
+  initCode?: BytesLike;
+  callData?: BytesLike;
+  callGasLimit?: BigNumberish;
+  verificationGasLimit?: BigNumberish;
+  preVerificationGas?: BigNumberish;
+  maxFeePerGas?: BigNumberish;
+  maxPriorityFeePerGas?: BigNumberish;
+  paymaster?: AddressLike;
+  paymasterData?: BytesLike;
+  signature?: BytesLike;
 }
 
 export interface PackedUserOperation {
-  sender: string;
-  nonce: number | string | bigint;
-  initCode: string;
-  callData: string;
-  accountGasLimits: string | number | bigint;
-  preVerificationGas: string | number | bigint;
-  gasFees: string | number | bigint;
-  paymasterAndData: string;
-  signature: string;
+  sender: AddressLike;
+  nonce: BigNumberish;
+  initCode: BytesLike;
+  callData: BytesLike;
+  accountGasLimits: BytesLike;
+  preVerificationGas: BigNumberish;
+  gasFees: BytesLike;
+  paymasterAndData: BytesLike;
+  signature: BytesLike;
+}
+
+export enum ExecutionMethod {
+  Execute,
+  ExecuteFromExecutor,
+  ExecuteUserOp,
+}
+
+export enum ModuleType {
+  Validation = 1,
+  Execution = 2,
+  Fallback = 3,
+  Hooks = 4,
 }
