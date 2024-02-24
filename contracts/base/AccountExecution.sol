9d6a57f1dedd30c9b6f043507c0426d904ba4e47
aboudjem <boudjemaa.adam@gmail.com>
2024-02-24 16:41:28 +0400

    🎨 simplify Execution funcs for quick test

--- contracts/base/AccountExecution.sol
+++ contracts/base/AccountExecution.sol
@@ -8,12 +8,9 @@ contract AccountExecution is IAccountExecution {
     /// @inheritdoc IAccountExecution
     function execute(bytes32 mode, bytes calldata executionCalldata) external payable {
         mode;
-
-        //@TODO make this via lib
-        address target = address(bytes20(executionCalldata[0:20]));
-        uint256 value = uint256(bytes32(executionCalldata[20:52]));
-        bytes calldata callData = executionCalldata[52:];
-        _execute(target, value, callData);
+        (address target, uint256 value, bytes memory callData) =
+            abi.decode(executionCalldata, (address, uint256, bytes));
+        target.call{ value: value }(callData);
     }
 
     /// @inheritdoc IAccountExecution
@@ -26,40 +23,14 @@ contract AccountExecution is IAccountExecution {
         returns (bytes[] memory returnData)
     {
         mode;
-        executionCalldata;
-        returnData = new bytes[](0);
-        return returnData;
+        (address target, uint256 value, bytes memory callData) =
+            abi.decode(executionCalldata, (address, uint256, bytes));
+        target.call{ value: value }(callData);
     }
 
     /// @inheritdoc IAccountExecution
     function executeUserOp(PackedUserOperation calldata userOp, bytes32 userOpHash) external payable {
         userOp;
         userOpHash;
-    }
-
-    function _execute(
-        /// @TODO : move to helper contract
-        address target,
-        uint256 value,
-        bytes calldata callData
-    )
-        internal
-        virtual
-        returns (bytes memory result)
-    {
-        /// @solidity memory-safe-assembly
-        assembly {
-            result := mload(0x40)
-            calldatacopy(result, callData.offset, callData.length)
-            if iszero(call(gas(), target, value, result, callData.length, codesize(), 0x00)) {
-                // Bubble up the revert if the call reverts.
-                returndatacopy(result, 0x00, returndatasize())
-                revert(result, returndatasize())
-            }
-            mstore(result, returndatasize()) // Store the length.
-            let o := add(result, 0x20)
-            returndatacopy(o, 0x00, returndatasize()) // Copy the returndata.
-            mstore(0x40, add(o, returndatasize())) // Allocate the memory.
-        }
     }
 }
