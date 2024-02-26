588e38e10ab483ca22b6ecb985fe4f6ecb4fcc55
aboudjem <boudjemaa.adam@gmail.com>
2024-02-26 23:04:24 +0100

    Add Forge-std Test import and refactor newWallet function

--- test/foundry/utils/CheatCodes.sol
+++ test/foundry/utils/CheatCodes.sol
@@ -2,24 +2,20 @@
 pragma solidity ^0.8.0;
 
 import "./Imports.sol";
+import "forge-std/src/Test.sol";
 
 contract CheatCodes is Test {
-
-       // Assign a readable name to an address to improve test output readability
+    // Assign a readable name to an address to improve test output readability
     function labelAddress(address addr, string memory name) internal {
         vm.label(addr, name);
     }
 
     // Create a new wallet from a given name, generating a private key, and label the address
-    function newWallet(string memory name) internal returns (address) {
-        // Derives a private key from a name, creates a wallet, and labels the account with that name
-        // VmSafe.Wallet memory wallet = vm.createWallet(name);
-        
-        // // Label the generated address for readability
-        // labelAddress(wallet.addr, name);
-        // return wallet.addr;
+    function newWallet(string memory name) internal returns (Vm.Wallet memory) {
+        Vm.Wallet memory wallet = vm.createWallet(name);
+        vm.label(wallet.addr, name);
+        return wallet;
     }
-
 
     function warpTo(uint256 timestamp) internal {
         vm.warp(timestamp);
@@ -38,12 +34,7 @@ contract CheatCodes is Test {
         vm.assume(condition);
     }
 
-    // function signMessage(address signer, bytes32 hash) internal returns (uint8 v, bytes32 r, bytes32 s) {
-    //     uint256 privateKey = uint256(keccak256(abi.encodePacked(signer)));
-    //     (v, r, s) = vm.sign(privateKey, hash);
-    // }
-
-        function startPrank(address addr) internal {
+    function startPrank(address addr) internal {
         vm.startPrank(addr);
     }
 
@@ -51,7 +42,7 @@ contract CheatCodes is Test {
         vm.stopPrank();
     }
 
-        function createSnapshot() internal returns (uint256) {
+    function createSnapshot() internal returns (uint256) {
         return vm.snapshot();
     }
 
@@ -65,7 +56,7 @@ contract CheatCodes is Test {
         }
     }
 
-       // Set the block base fee
+    // Set the block base fee
     function setBaseFee(uint256 baseFee) internal {
         vm.fee(baseFee);
     }
@@ -74,8 +65,12 @@ contract CheatCodes is Test {
         return vm.load(contractAddress, slot);
     }
 
-       // Set contract code for an address
+    // Set contract code for an address
     function setContractCode(address contractAddress, bytes memory code) internal {
         vm.etch(contractAddress, code);
+    }
+
+    function test(uint256 a) public {
+        a;
     }
 }
