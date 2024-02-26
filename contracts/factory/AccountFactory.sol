b3cff4439e97a17fe847915a63ebbf2883c880c3
aboudjem <boudjemaa.adam@gmail.com>
2024-02-26 22:52:03 +0100

    ✨ Add computeAccountAddress function to AccountFactory

--- contracts/factory/AccountFactory.sol
+++ contracts/factory/AccountFactory.sol
@@ -15,8 +15,28 @@ contract AccountFactory is IAccountFactory, StakeManager {
         if (account.code.length > 0) {
             return account;
         }
-
         account = Create2.deploy(0, salt, bytecode);
         IModuleConfig(account).installModule(index, module, data);
+    }
+
+    /**
+     * @dev Computes the expected address of a SmartAccount contract created via the factory.
+     * @param module The address of the module to be used in the SmartAccount.
+     * @param index The index or type of the module, for differentiation if needed.
+     * @param data The initialization data for the module.
+     * @return expectedAddress The address at which the new SmartAccount contract will be deployed.
+     */
+    function computeAccountAddress(
+        address module,
+        uint256 index,
+        bytes calldata data
+    )
+        external
+        view
+        returns (address expectedAddress)
+    {
+        bytes32 salt = keccak256(abi.encodePacked(module, index, data));
+        bytes memory bytecode = abi.encodePacked(type(SmartAccount).creationCode);
+        expectedAddress = Create2.computeAddress(salt, keccak256(bytecode));
     }
 }
