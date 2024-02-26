3384fae4aaa74e531e11aff39b3644ee04bfb899
aboudjem <boudjemaa.adam@gmail.com>
2024-02-26 23:04:04 +0100

    Refactor BicoTestBase and import Helpers and console2.sol

--- test/foundry/utils/BicoTestBase.t.sol
+++ test/foundry/utils/BicoTestBase.t.sol
@@ -1,131 +1,40 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.24 <0.9.0;
 
-import "./Imports.sol";
-import "forge-std/console2.sol";
 
-import { Vm } from "forge-std/Vm.sol";
+import "./Helpers.sol";
+import "forge-std/src/console2.sol";
 
-contract BicoTestBase is PRBTest, Imports {
-    enum ModuleType {
-        Validation,
-        Execution,
-        Fallback,
-        Hooks
-    }
-
-    IEntryPoint entrypoint;
-
+contract BicoTestBase is Helpers {
     SmartAccount public implementation;
     SmartAccount public smartAccount;
-    AccountFactory public factory;
 
-    MockValidator public mockValidator;
-
-    address target;
-    address payable alice;
-    address owner;
-
-    function setUp() public virtual {
+    function init() public {
+        setAddress();
         implementation = new SmartAccount();
-        entrypoint = new EntryPoint();
-
-        factory = new AccountFactory();
-
-        //@TODO: deploy account via Factory and EP
-        // smartAccount = new SmartAccount();
-
-        mockValidator = new MockValidator();
-
-        target = address(0x69);
-        alice = payable(address(0xa11ce));
-        owner = address(0xff);
-        vm.deal(address(0xff), 1000 ether);
-        vm.deal(address(owner), 1000 ether);
-        vm.deal(address(smartAccount), 1000 ether);
     }
 
-    // HELPERS
-    function _getNonce(address account, address validator) internal returns (uint256 nonce) {
-        uint192 key = uint192(bytes24(bytes20(address(validator))));
-        nonce = entrypoint.getNonce(address(account), key);
-    }
+    function deploySmartAccount(Vm.Wallet memory wallet) public returns (address) {
+        address account = getAccountAddress(wallet.addr);
+        address signer = wallet.addr;
 
-    function testBuildPackedUserOp() public {
-        vm.startPrank(owner);
-        address signer = owner;
-        // IAccountFactory;
-        bytes memory tx = abi.encodeWithSignature(
-            "createAccount(address,uint256,bytes)", address(mockValidator), uint256(ModuleType.Validation), signer
-        );
-
-        bytes memory initCode = abi.encode(address(factory), tx);
-        initCode = abi.encodeCall(
-            factory.createAccount, (address(mockValidator), uint256(ModuleType.Validation), abi.encodePacked(signer))
-        );
-        address account = _getAccountAddress(signer);
+        bytes memory initCode = createInitCode(wallet.addr, FACTORY.createAccount.selector);
 
         PackedUserOperation[] memory userOps = new PackedUserOperation[](1);
-        userOps[0] = PackedUserOperation({
-            sender: address(0),
-            nonce: _getNonce(account, address(mockValidator)),
-            initCode: initCode,
-            callData: "",
-            accountGasLimits: bytes32(abi.encodePacked(uint128(1e6), uint128(1e6))),
-            preVerificationGas: 1e6,
-            gasFees: bytes32(abi.encodePacked(uint128(1e6), uint128(1e6))),
-            paymasterAndData: "",
-            signature: ""
-        });
+        userOps[0] = buildPackedUserOp(account, _getNonce(account, address(VALIDATOR_MODULE)));
 
-        // bytes32 userOpHash = entrypoint.getUserOpHash(userOps[0]);
+        userOps[0].initCode = initCode;
 
-        // (uint8 v, bytes32 r, bytes32 s) = vm.sign(signer.privateKey, userOpHash);
-        // userOps[0].signature = abi.encodePacked(r, s, v);
+        bytes32 userOpHash = ENTRYPOINT.getUserOpHash(userOps[0]);
 
-        // // bytes memory signedHash = abi.encode(ECDSA.toEthSignedMessageHash(hash));
-
-        // // userOps[0].signature = signedHash;
-
-        // entrypoint.depositTo{ value: 100 ether }(account);
-        // // entrypoint.depositTo{value: 1000 ether}(owner);
-        // entrypoint.handleOps(userOps, payable(owner));
+        userOps[0].signature = signMessageAndGetSignatureBytes(wallet, userOpHash);
+        ENTRYPOINT.depositTo{ value: 100 ether }(account);
+        ENTRYPOINT.handleOps(userOps, payable(wallet.addr));
+        return account;
     }
 
-    function _getAccountAddress(address signer) internal view returns (address account) {
-        address factoryAddress = address(factory);
-        address mockValidatorAddress = address(mockValidator);
-        uint256 moduleType = uint256(ModuleType.Validation);
 
-        (,, account) = _computeAddress(signer, mockValidatorAddress, factoryAddress, moduleType);
-    }
-
-    function _computeAddress(
-        address signer,
-        address mockValidatorAddress,
-        address factoryAddress,
-        uint256 moduleType
-    )
-        internal
-        view
-        returns (bytes memory, bytes32, address)
-    {
-        bytes memory initData = abi.encodePacked(signer);
-        bytes32 salt = keccak256(abi.encodePacked(mockValidatorAddress, moduleType, initData));
-        bytes memory bytecode = type(SmartAccount).creationCode;
-        address account = Create2.computeAddress(salt, keccak256(bytecode));
-        return (bytecode, salt, account);
+    function testBico(uint256 a) public {
+        a;
     }
 }
-
-// struct PackedUserOperation {
-//     address sender;
-//     uint256 nonce;
-//     bytes initCode;
-//     bytes callData;
-//     bytes32 accountGasLimits;
-//     uint256 preVerificationGas;
-//     bytes32 gasFees;    //maxPriorityFee and maxFeePerGas;
-//     bytes paymasterAndData;
-//     bytes signature;
-// }
