6a2a5ecac8582f6d21c00036ef9842c22db9ebfa
aboudjem <boudjemaa.adam@gmail.com>
2024-02-26 23:02:37 +0100

    Update imports in Imports.sol

--- test/foundry/utils/Imports.sol
+++ test/foundry/utils/Imports.sol
@@ -1,36 +1,46 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-// Importing interfaces
+// Standard library imports
+import "forge-std/src/console2.sol";
+import "forge-std/src/Test.sol";
+import "forge-std/src/Vm.sol";
+
+// Utility libraries
+import { ECDSA } from "solady/src/utils/ECDSA.sol";
+import { MessageHashUtils } from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+import { PRBTest } from "@prb/test/src/PRBTest.sol";
+
+// Account Abstraction imports
+import { EntryPoint } from "account-abstraction/contracts/core/EntryPoint.sol";
+import { IEntryPoint } from "account-abstraction/contracts/interfaces/IEntryPoint.sol";
+import { PackedUserOperation } from "account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+
+// Interface imports
 import "contracts/interfaces/base/IAccountConfig.sol";
 import "contracts/interfaces/base/IAccountExecution.sol";
 import "contracts/interfaces/base/IModuleManager.sol";
 import "contracts/interfaces/IModule.sol";
-import "contracts/interfaces/IStorage.sol";
+import "contracts/interfaces/base/IStorage.sol";
 import "contracts/interfaces/factory/IAccountFactory.sol";
 
-// Importing contract implementations
+// Contract implementations
 import "contracts/base/AccountConfig.sol";
 import "contracts/base/AccountExecution.sol";
 import "contracts/base/ModuleManager.sol";
 import "contracts/SmartAccount.sol";
 import "contracts/factory/AccountFactory.sol";
 
-// Importing Mock contracts
+// Mock contracts for testing
 import "contracts/test/mocks/MockValidator.sol";
+import "contracts/test/mocks/Counter.sol";
 
-import { EntryPoint } from "account-abstraction/contracts/core/EntryPoint.sol";
-import { IEntryPoint } from "account-abstraction/contracts/interfaces/IEntryPoint.sol";
-import { PackedUserOperation } from "account-abstraction/contracts/interfaces/PackedUserOperation.sol";
-import { PRBTest } from "@prb/test/src/PRBTest.sol";
+// Helper and Struct imports
+import "./Structs.sol";
+import "./Helpers.sol";
 
-import { ECDSA } from "solady/src/utils/ECDSA.sol";
-import { MessageHashUtils } from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
-import "forge-std/Vm.sol";
-import "forge-std/console2.sol";
-import "forge-std/Test.sol";
 contract Imports {
-// This contract acts as a single point of import for Foundry tests.
-// It does not require any logic, as its sole purpose is to consolidate imports.
-// You can extend this contract in your test files to access all imported contracts.
+    // This contract acts as a single point of import for Foundry tests.
+    // It does not require any logic, as its sole purpose is to consolidate imports.
+    // You can extend this contract in your test files to access all imported contracts and libraries.
 }
