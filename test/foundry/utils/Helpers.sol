765f42c862699f7285dc150d56827a63670367ea
aboudjem <boudjemaa.adam@gmail.com>
2024-03-13 10:30:34 +0100

    Add imports and fix code in Helpers.sol

--- test/foundry/utils/Helpers.sol
+++ test/foundry/utils/Helpers.sol
@@ -4,12 +4,20 @@ pragma solidity ^0.8.0;
 import "./CheatCodes.sol";
 import "./Imports.sol";
 import { IEntryPoint } from "account-abstraction/contracts/interfaces/IEntryPoint.sol";
+import { EntryPoint } from "account-abstraction/contracts/core/EntryPoint.sol";
 
 import { PackedUserOperation } from "account-abstraction/contracts/interfaces/PackedUserOperation.sol";
 import { AccountFactory } from "../../../contracts/factory/AccountFactory.sol";
 import { MockValidator } from "../mocks/MockValidator.sol";
 import { SmartAccount } from "../../../contracts/SmartAccount.sol";
+import "../../../contracts/lib/ModeLib.sol";
+import "../../../contracts/lib/ExecLib.sol";
+import "../../../contracts/lib/ModuleTypeLib.sol";
 
+import {AccountExecution} from "../../../contracts/base/AccountExecution.sol";
+
+import "solady/src/utils/ECDSA.sol";
+import "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 contract Helpers is CheatCodes {
     // Pre-defined roles
     Vm.Wallet public DEPLOYER;
@@ -93,6 +101,18 @@ contract Helpers is CheatCodes {
         setContractCode(newAddress, originalAddress.code);
     }
 
+    function getAccountAddress(address signer) internal view returns (address payable account) {
+        bytes memory initData = abi.encodePacked(signer);
+
+        uint256 moduleTypeId = uint256(0);
+
+        uint256 saDeploymentIndex = 0;
+
+        account = FACTORY.getCounterFactualAddress(address(VALIDATOR_MODULE), initData, saDeploymentIndex);
+
+        return account;
+    }
+
     // Helper to build a user operation struct for account abstraction tests
     function buildPackedUserOp(address sender, uint256 nonce) internal pure returns (PackedUserOperation memory) {
         return PackedUserOperation({
@@ -121,41 +141,6 @@ contract Helpers is CheatCodes {
         signature = abi.encodePacked(r, s, v);
     }
 
-    function getAccountAddress(address signer) internal view returns (address payable account) {
-        bytes memory initData = abi.encodePacked(signer);
-
-        uint256 moduleTypeId = uint256(MODULE_TYPE_VALIDATOR);
-
-        uint256 saDeploymentIndex = 0;
-
-        account = FACTORY.getCounterFactualAddress(address(VALIDATOR_MODULE), initData, saDeploymentIndex);
-
-        return account;
-    }
-    // Method to create a UserOperation
-
-    function createUserOperation(
-        Vm.Wallet memory wallet,
-        address module
-    )
-        internal
-        returns (PackedUserOperation memory userOp)
-    {
-        address accountAddress = getAccountAddress(wallet.addr);
-
-        // Constructing the UserOperation with the signed hash
-        userOp = PackedUserOperation({
-            sender: accountAddress,
-            nonce: getNonce(accountAddress, module),
-            initCode: "",
-            callData: "",
-            accountGasLimits: bytes32(abi.encodePacked(uint128(3e6), uint128(3e6))),
-            preVerificationGas: 3e6,
-            gasFees: bytes32(abi.encodePacked(uint128(3e6), uint128(3e6))),
-            paymasterAndData: "",
-            signature: ""
-        });
-    }
 
     function createInitCode(
         address ownerAddress,
@@ -166,7 +151,7 @@ contract Helpers is CheatCodes {
         returns (bytes memory initCode)
     {
         address module = address(VALIDATOR_MODULE);
-        uint256 moduleTypeId = uint256(MODULE_TYPE_VALIDATOR);
+        uint256 moduleTypeId = uint256(0);
         uint256 saDeploymentIndex = 0;
         bytes memory moduleInitData = abi.encodePacked(ownerAddress);
 
