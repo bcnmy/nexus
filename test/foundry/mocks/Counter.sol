c2e014d550fee3ce489a7f1b447f5d13f1a11ba5
aboudjem <boudjemaa.adam@gmail.com>
2024-03-14 02:56:34 +0100

    Add revertOperation function to Counter.sol

--- test/foundry/mocks/Counter.sol
+++ test/foundry/mocks/Counter.sol
@@ -12,11 +12,11 @@ contract Counter {
         _number--;
     }
 
-    /**
-     * @dev Return value
-     * @return value of 'number'
-     */
     function getNumber() public view returns (uint256) {
         return _number;
+    }
+
+    function revertOperation() public pure {
+        revert("Counter: Revert operation");
     }
 }
