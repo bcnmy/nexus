7922df0e0a4e2b01ae32d2311f536a519eeeb162
aboudjem <boudjemaa.adam@gmail.com>
2024-03-14 02:56:56 +0100

    Add TestAccountExecution_ExecuteFromExecutor unit test

--- test/foundry/unit/concrete/accountexecution/TestAccountExecution_ExecuteFromExecutor.t.sol
+++ test/foundry/unit/concrete/accountexecution/TestAccountExecution_ExecuteFromExecutor.t.sol
@@ -1,1 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "../../../utils/Imports.sol";
+import "../../../utils/BicoTestBase.t.sol";
+import { MockExecutor } from "../../../mocks/MockExecutor.sol";
+import { Counter } from "../../../mocks/Counter.sol";
+
+error InvalidModule(address module);
+
+contract TestAccountExecution_ExecuteFromExecutor is Test, BicoTestBase {
+    SmartAccount public BOB_ACCOUNT;
+    MockExecutor public mockExecutor;
+    Counter public counter;
+
+    function setUp() public {
+        init();
+        BOB_ACCOUNT = SmartAccount(deploySmartAccount(BOB));
+        mockExecutor = new MockExecutor();
+        counter = new Counter();
+
+        // Install MockExecutor as executor module on BOB_ACCOUNT
+        bytes memory callDataInstall = abi.encodeWithSelector(
+            IModuleManager.installModule.selector,
+            uint256(2),
+            address(mockExecutor),
+            ""
+        );
+        PackedUserOperation[] memory userOpsInstall = prepareExecutionUserOp(
+            BOB,
+            BOB_ACCOUNT,
+            ModeLib.encodeSimpleSingle(),
+            address(BOB_ACCOUNT),
+            0,
+            callDataInstall
+        );
+        ENTRYPOINT.handleOps(userOpsInstall, payable(address(BOB.addr)));
+    }
 
