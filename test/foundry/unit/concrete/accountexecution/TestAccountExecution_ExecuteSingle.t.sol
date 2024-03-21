0348611bb2d0e24c265c571bbef13cff10b4a97c
aboudjem <boudjemaa.adam@gmail.com>
2024-03-22 00:14:27 +0100

    Refactor user operation preparation in TestAccountExecution_ExecuteSingle

--- test/foundry/unit/concrete/accountexecution/TestAccountExecution_ExecuteSingle.t.sol
+++ test/foundry/unit/concrete/accountexecution/TestAccountExecution_ExecuteSingle.t.sol
@@ -12,14 +12,17 @@ contract TestAccountExecution_ExecuteSingle is TestAccountExecution_Base {
         // Initial state assertion
         assertEq(counter.getNumber(), 0, "Counter should start at 0");
 
+        Execution[] memory execution = new Execution[](1);
+        execution[0] = Execution( address(counter),
+            0,
+            abi.encodeWithSelector(Counter.incrementNumber.selector));
+
         // Assuming you have a method to prepare a UserOperation for a single execution
-        PackedUserOperation[] memory userOps = prepareExecutionUserOp(
+        PackedUserOperation[] memory userOps = prepareUserOperation(
             BOB, // Assuming `BOB` is defined in your base test setup
             BOB_ACCOUNT, // Assuming `BOB_ACCOUNT` is defined in your base test setup
             EXECTYPE_DEFAULT,
-            address(counter),
-            0,
-            abi.encodeWithSelector(Counter.incrementNumber.selector)
+            execution
         );
 
         // Execute the single operation
@@ -33,15 +36,15 @@ contract TestAccountExecution_ExecuteSingle is TestAccountExecution_Base {
         // Initial state assertion
         assertEq(counter.getNumber(), 0, "Counter should start at 0");
 
-        // Assuming you have a method to prepare a UserOperation for a single execution that should fail
-        PackedUserOperation[] memory userOps = prepareExecutionUserOp(
+       Execution[] memory execution = new Execution[](1);
+       execution[0] = Execution(address(counter), 0, abi.encodeWithSelector(Counter.revertOperation.selector));
+
+        // Assuming the method should fail
+        PackedUserOperation[] memory userOps = prepareUserOperation(
             BOB,
             BOB_ACCOUNT,
             EXECTYPE_DEFAULT,
-            address(counter),
-            0,
-            abi.encodeWithSelector(Counter.revertOperation.selector) // Assuming `revertOperation` causes a revert
-        );
+            execution);
         bytes32 userOpHash = ENTRYPOINT.getUserOpHash(userOps[0]);
 
         bytes memory expectedRevertReason = abi.encodeWithSignature("Error(string)", "Counter: Revert operation");
@@ -63,14 +66,16 @@ contract TestAccountExecution_ExecuteSingle is TestAccountExecution_Base {
     }
 
     function test_ExecuteSingle_Empty() public {
+
+        Execution[] memory execution = new Execution[](1);
+        execution[0] = Execution(address(0), 0, "");
+
         // Build UserOperation for single execution
-        PackedUserOperation[] memory userOps = prepareExecutionUserOp(
+        PackedUserOperation[] memory userOps = prepareUserOperation(
             BOB,
             BOB_ACCOUNT,
             EXECTYPE_DEFAULT,
-            address(0),
-            0,
-            ""
+            execution
         );
 
         ENTRYPOINT.handleOps(userOps, payable(address(BOB.addr)));
@@ -83,17 +88,17 @@ contract TestAccountExecution_ExecuteSingle is TestAccountExecution_Base {
         // Fund BOB_ACCOUNT with 2 ETH to cover the value transfer
         payable(address(BOB_ACCOUNT)).call{ value: 2 ether }(""); // Fund BOB_ACCOUNT
 
+        Execution[] memory execution = new Execution[](1);
+        execution[0] = Execution(receiver, sendValue, "");
 
         assertEq(receiver.balance, 0, "Receiver should have 0 ETH");
 
         // Build UserOperation for single execution
-        PackedUserOperation[] memory userOps = prepareExecutionUserOp(
+        PackedUserOperation[] memory userOps = prepareUserOperation(
             BOB,
             BOB_ACCOUNT,
             EXECTYPE_DEFAULT,
-            receiver,
-            sendValue,
-            ""
+            execution
         );
 
         ENTRYPOINT.handleOps(userOps, payable(BOB.addr));
