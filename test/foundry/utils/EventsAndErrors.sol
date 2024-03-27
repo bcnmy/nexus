d7f9674015359c640baba979be6b2f0f61948484
aboudjem <boudjemaa.adam@gmail.com>
2024-03-27 03:39:25 +0100

    Add events and errors to EventsAndErrors.sol

--- test/foundry/utils/EventsAndErrors.sol
+++ test/foundry/utils/EventsAndErrors.sol
@@ -5,10 +5,20 @@ contract EventsAndErrors {
     // Define all events
     event ModuleInstalled(uint256 moduleTypeId, address module);
     event ModuleUninstalled(uint256 moduleTypeId, address module);
-    
     event UserOperationRevertReason(bytes32 indexed userOpHash, address indexed sender, uint256 nonce, bytes revertReason);
+    event AccountCreated(address indexed account, address indexed validationModule, bytes moduleInstallData);
+
 
 
     // Define all errors
     error InvalidModule(address module);
+    error FailedOp(uint256 opIndex, string reason);
+    error AccountInitializationFailed();
+    error AccountAccessUnauthorized();
+    error ExecutionFailed();
+    error ModuleAlreadyInstalled(uint256 moduleTypeId, address module);
+    error AlreadyInitialized(address smartAccount);
+    error NotInitialized(address smartAccount);
+    error LinkedList_AlreadyInitialized();
+    error LinkedList_InvalidPage();
 }
