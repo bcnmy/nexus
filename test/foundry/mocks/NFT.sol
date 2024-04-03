eab7cc9fb375978f565f9499b3d0e5e4c5e6b11f
aboudjem <boudjemaa.adam@gmail.com>
2024-04-03 17:17:39 +0200

    Add mint and safeMint functions for testing purposes

--- test/foundry/mocks/NFT.sol
+++ test/foundry/mocks/NFT.sol
@@ -7,4 +7,16 @@ contract NFT is ERC721 {
     constructor(string memory name, string memory symbol) ERC721(name, symbol) {
         _mint(msg.sender, 10);
     }
+
+    // Mint a new NFT token to the specified address with the specified tokenId
+    // Warning: This function is only for testing purposes and should not be used in production
+    function mint(address to, uint256 tokenId) public {
+        _mint(to, tokenId);
+    }
+
+    // Safely mint a new NFT token to the specified address with the specified tokenId
+    // Warning: This function is only for testing purposes and should not be used in production
+    function safeMint(address to, uint256 tokenId) public {
+        _safeMint(to, tokenId);
+    }
 }
