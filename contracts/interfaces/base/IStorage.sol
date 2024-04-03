21952932121cc1e3cfdd6a8756c473828265cb4b
aboudjem <boudjemaa.adam@gmail.com>
2024-04-03 17:13:27 +0200

    Add fallback handler mapping to IStorage interface

--- contracts/interfaces/base/IStorage.sol
+++ contracts/interfaces/base/IStorage.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.24;
 
 import { SentinelListLib } from "sentinellist/src/SentinelList.sol";
+import { CallType } from "../../lib/ModeLib.sol";
+
 import { IHook } from "../modules/IHook.sol";
 
 interface IStorage {
@@ -9,10 +11,18 @@ interface IStorage {
     struct AccountStorage {
         // linked list of validators. List is initialized by initializeAccount()
         SentinelListLib.SentinelList validators;
+        
         // linked list of executors. List is initialized by initializeAccount()
         SentinelListLib.SentinelList executors;
-        // single fallback handler for all fallbacks
-        address fallbackHandler;
+        
+        // mapping of selector to fallback handler
+        mapping(bytes4 selector => FallbackHandler fallbackHandler) fallbacks;
+
         IHook hook;
+    }
+
+    struct FallbackHandler {
+        address handler;
+        CallType calltype;
     }
 }
