51583aeee875b09dc1724ffc3ff7b5c6eeda033d
aboudjem <boudjemaa.adam@gmail.com>
2024-04-03 17:14:22 +0200

    Refactor MockHandler.sol: Remove unused imports and implement event

--- test/foundry/mocks/MockHandler.sol
+++ test/foundry/mocks/MockHandler.sol
@@ -3,22 +3,23 @@ pragma solidity ^0.8.23;
 
 /* solhint-disable no-empty-blocks */
 
-import { IERC1155Receiver } from "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
 import { IERC721Receiver } from "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import { IERC165 } from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 import { IFallback, MODULE_TYPE_FALLBACK } from "contracts/interfaces/modules/IERC7579Modules.sol";
 import { IModule } from "contracts/interfaces/modules/IModule.sol";
 import { EncodedModuleTypes } from "contracts/lib/ModuleTypeLib.sol";
+import "../utils/EventsAndErrors.sol";
 
 /**
  * @title Default Handler - returns true for known token callbacks
  *   @dev May Handle EIP-1271 compliant isValidSignature requests.
  *  @notice inspired by Richard Meissner's <richard@gnosis.pm> implementation
  */
-contract MockHandler is IERC165, IERC1155Receiver, IERC721Receiver, IFallback {
+contract MockHandler is IERC165, IERC721Receiver, IFallback {
     string public constant NAME = "Default Handler";
     string public constant VERSION = "1.0.0";
 
+    event FallbackHandlerTriggered();
     error NonExistingMethodCalled(bytes4 selector);
 
     fallback() external {
@@ -31,53 +32,16 @@ contract MockHandler is IERC165, IERC1155Receiver, IERC721Receiver, IFallback {
      * @return True if the contract implements the given interface, false otherwise.
      */
     function supportsInterface(bytes4 interfaceId) external view virtual override returns (bool) {
-        return interfaceId == type(IERC1155Receiver).interfaceId || interfaceId == type(IERC721Receiver).interfaceId
+        return interfaceId == type(IERC721Receiver).interfaceId
             || interfaceId == type(IERC165).interfaceId;
-    }
-
-    /**
-     * @dev Handles the receipt of a single ERC1155 token type.
-     * @return The interface selector for the called function.
-     */
-    function onERC1155Received(
-        address,
-        address,
-        uint256,
-        uint256,
-        bytes calldata
-    )
-        external
-        pure
-        override
-        returns (bytes4)
-    {
-        return IERC1155Receiver.onERC1155Received.selector;
-    }
-
-    /**
-     * @dev Handles the receipt of multiple ERC1155 token types.
-     * @return The interface selector for the called function.
-     */
-    function onERC1155BatchReceived(
-        address,
-        address,
-        uint256[] calldata,
-        uint256[] calldata,
-        bytes calldata
-    )
-        external
-        pure
-        override
-        returns (bytes4)
-    {
-        return IERC1155Receiver.onERC1155BatchReceived.selector;
     }
 
     /**
      * @dev Handles the receipt of an ERC721 token.
      * @return The interface selector for the called function.
      */
-    function onERC721Received(address, address, uint256, bytes calldata) external pure override returns (bytes4) {
+    function onERC721Received(address, address, uint256, bytes calldata) external override returns (bytes4) {
+        emit FallbackHandlerTriggered();
         return IERC721Receiver.onERC721Received.selector;
     }
 
