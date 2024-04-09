b9b2ec31a64241ce44e19def630fac18b986c88c
Filipp Makarov <filmakarov@Filipps-MacBook-Pro-2.local>
2024-04-09 10:52:24 +0300

    rename var

--- test/foundry/unit/concrete/erc4337account/TestERC4337Account_AddDeposit.t.sol
+++ test/foundry/unit/concrete/erc4337account/TestERC4337Account_AddDeposit.t.sol
@@ -19,10 +19,10 @@ contract TestERC4337Account_addDeposit is Test, SmartAccountTestLab {
     }
 
     function test_AddDeposit_Success() public {
-        uint256 balanceBefore = ENTRYPOINT.balanceOf(address(account));
+        uint256 depositBefore = ENTRYPOINT.balanceOf(address(account));
         account.addDeposit{ value: defaultDepositAmount }();
         assertEq(
-            balanceBefore + defaultDepositAmount,
+            depositBefore + defaultDepositAmount,
             ENTRYPOINT.balanceOf(address(account)),
             "Deposit should be added to EntryPoint"
         );
@@ -43,7 +43,7 @@ contract TestERC4337Account_addDeposit is Test, SmartAccountTestLab {
 
     function test_AddDeposit_DepositViaHandleOps() public {
         _prefundSmartAccountAndAssertSuccess(address(account), defaultDepositAmount + 1 ether);
-        uint256 balanceBefore = ENTRYPOINT.balanceOf(address(account));
+        uint256 depositBefore = ENTRYPOINT.balanceOf(address(account));
 
         Execution[] memory executions = _prepareSingleExecution(address(account), defaultDepositAmount, abi.encodeWithSignature("addDeposit()"));
         PackedUserOperation[] memory userOps = prepareUserOperation(BOB, account, EXECTYPE_DEFAULT, executions);
@@ -51,7 +51,7 @@ contract TestERC4337Account_addDeposit is Test, SmartAccountTestLab {
 
         // Using almostEq to compare balances with a tolerance for gas costs
         almostEq(
-            balanceBefore + defaultDepositAmount - (gasUsed * tx.gasprice),
+            depositBefore + defaultDepositAmount - (gasUsed * tx.gasprice),
             ENTRYPOINT.balanceOf(address(account)),
             defaultMaxPercentDelta
         );
@@ -60,14 +60,14 @@ contract TestERC4337Account_addDeposit is Test, SmartAccountTestLab {
     function test_AddDeposit_BatchDepositViaHandleOps() public {
         uint256 executionsNumber = 5;
         _prefundSmartAccountAndAssertSuccess(address(account), defaultDepositAmount * 10);
-        uint256 balanceBefore = ENTRYPOINT.balanceOf(address(account));
+        uint256 depositBefore = ENTRYPOINT.balanceOf(address(account));
 
         Execution memory execution = Execution(address(account), defaultDepositAmount, abi.encodeWithSignature("addDeposit()"));
         Execution[] memory executions = prepareSeveralIdenticalExecutions(execution, executionsNumber);
         PackedUserOperation[] memory userOps = prepareUserOperation(BOB, account, EXECTYPE_DEFAULT, executions);
         uint256 gasUsed = handleUserOpAndMeasureGas(userOps, BOB.addr);
         almostEq(
-            balanceBefore + (defaultDepositAmount * executionsNumber) - (gasUsed * tx.gasprice),
+            depositBefore + (defaultDepositAmount * executionsNumber) - (gasUsed * tx.gasprice),
             ENTRYPOINT.balanceOf(address(account)),
             defaultMaxPercentDelta
         );
@@ -75,14 +75,14 @@ contract TestERC4337Account_addDeposit is Test, SmartAccountTestLab {
 
     function test_AddDeposit_Try_DepositViaHandleOps() public {
         _prefundSmartAccountAndAssertSuccess(address(account), defaultDepositAmount + 1 ether);
-        uint256 balanceBefore = ENTRYPOINT.balanceOf(address(account));
+        uint256 depositBefore = ENTRYPOINT.balanceOf(address(account));
 
         Execution[] memory executions = _prepareSingleExecution(address(account), defaultDepositAmount, abi.encodeWithSignature("addDeposit()"));
         PackedUserOperation[] memory userOps = prepareUserOperation(BOB, account, EXECTYPE_TRY, executions);
         uint256 gasUsed = handleUserOpAndMeasureGas(userOps, BOB.addr);
 
         almostEq(
-            balanceBefore + defaultDepositAmount - (gasUsed * tx.gasprice),
+            depositBefore + defaultDepositAmount - (gasUsed * tx.gasprice),
             ENTRYPOINT.balanceOf(address(account)),
             defaultMaxPercentDelta
         );
@@ -90,7 +90,7 @@ contract TestERC4337Account_addDeposit is Test, SmartAccountTestLab {
 
     function test_AddDeposit_Try_BatchDepositViaHandleOps() public {
         _prefundSmartAccountAndAssertSuccess(address(account), defaultDepositAmount * 10);
-        uint256 balanceBefore = ENTRYPOINT.balanceOf(address(account));
+        uint256 depositBefore = ENTRYPOINT.balanceOf(address(account));
         uint256 executionsNumber = 5;
 
         Execution memory execution = Execution(address(account), defaultDepositAmount, abi.encodeWithSignature("addDeposit()"));
@@ -99,7 +99,7 @@ contract TestERC4337Account_addDeposit is Test, SmartAccountTestLab {
         uint256 gasUsed = handleUserOpAndMeasureGas(userOps, BOB.addr);
 
         almostEq(
-            balanceBefore + (defaultDepositAmount * executionsNumber) - (gasUsed * tx.gasprice),
+            depositBefore + (defaultDepositAmount * executionsNumber) - (gasUsed * tx.gasprice),
             ENTRYPOINT.balanceOf(address(account)),
             defaultMaxPercentDelta
         );
