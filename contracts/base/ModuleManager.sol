9556d481a04a8c9b8f8b9d805e5f59781eac7f1e
aboudjem <boudjemaa.adam@gmail.com>
2024-04-23 17:10:54 +0400

    add natspec

--- contracts/base/ModuleManager.sol
+++ contracts/base/ModuleManager.sol
@@ -19,7 +19,7 @@ import { CallType, CALLTYPE_SINGLE, CALLTYPE_STATIC } from "../lib/ModeLib.sol";
  * shoutout to zeroknots (rhinestone.wtf) for this code
  * NOTE: the linked list is just an example. accounts may implement this differently
  */
-abstract contract ModuleManager is Storage, Receiver {
+contract ModuleManager is Storage, Receiver {
     error CannotRemoveLastValidator();
     error InvalidModule(address module);
     error InvalidModuleTypeId(uint256 moduleTypeId);
@@ -39,6 +39,7 @@ abstract contract ModuleManager is Storage, Receiver {
 
     using SentinelListLib for SentinelListLib.SentinelList;
 
+    /// @dev Retrieves and prepares the hook for pre-check operations.
     function _preCheck() internal returns (address hook, bytes memory hookData) {
         hook = _getHook();
         if (hook != address(0)) {
@@ -47,6 +48,7 @@ abstract contract ModuleManager is Storage, Receiver {
         }
     }
 
+    /// @dev Retrieves and prepares the hook for post-check operations.
     function _postCheck(
         address hook,
         bytes memory hookData,
@@ -58,18 +60,21 @@ abstract contract ModuleManager is Storage, Receiver {
         }
     }
 
+    /// @notice Ensures the message sender is a registered executor module.
     modifier onlyExecutorModule() virtual {
         SentinelListLib.SentinelList storage executors = _getAccountStorage().executors;
         if (!executors.contains(msg.sender)) revert InvalidModule(msg.sender);
         _;
     }
 
+    /// @notice Ensures the specified address is a registered validator module.
     modifier onlyValidatorModule(address validator) virtual {
         SentinelListLib.SentinelList storage validators = _getAccountStorage().validators;
         if (!validators.contains(validator)) revert InvalidModule(validator);
         _;
     }
 
+    /// @dev Fallback function to manage incoming calls using designated handlers based on the call type.
     fallback() external payable override(Receiver) receiverFallback {
         FallbackHandler storage $fallbackHandler = _getAccountStorage().fallbacks[msg.sig];
         address handler = $fallbackHandler.handler;
@@ -130,37 +135,50 @@ abstract contract ModuleManager is Storage, Receiver {
         }
     }
 
-    /**
-     * THIS IS NOT PART OF THE STANDARD
-     * Helper Function to access linked list
-     */
+    /// @dev Retrieves a paginated list of validator addresses from the linked list.
+    /// This utility function is not defined by the ERC-7579 standard and is implemented to facilitate
+    /// easier management and retrieval of large sets of validator modules.
+    /// @param cursor The address to start pagination from, or zero to start from the first entry.
+    /// @param size The number of validator addresses to return.
+    /// @return array An array of validator addresses.
+    /// @return next The address to use as a cursor for the next page of results.
     function getValidatorsPaginated(
         address cursor,
         uint256 size
-    ) external view virtual returns (address[] memory array, address next) {
-        (array, next) = _getValidatorsPaginated(cursor, size);
+    ) external view returns (address[] memory array, address next) {
+        (array, next) = _paginate(_getAccountStorage().validators, cursor, size);
     }
 
-    /**
-     * THIS IS NOT PART OF THE STANDARD
-     * Helper Function to access linked list
-     */
+    /// @dev Retrieves a paginated list of executor addresses from the linked list.
+    /// This utility function is not defined by the ERC-7579 standard and is implemented to facilitate
+    /// easier management and retrieval of large sets of executor modules.
+    /// @param cursor The address to start pagination from, or zero to start from the first entry.
+    /// @param size The number of executor addresses to return.
+    /// @return array An array of executor addresses.
+    /// @return next The address to use as a cursor for the next page of results.
     function getExecutorsPaginated(
         address cursor,
         uint256 size
-    ) external view virtual returns (address[] memory array, address next) {
-        (array, next) = _getExecutorsPaginated(cursor, size);
+    ) external view returns (address[] memory array, address next) {
+        (array, next) = _paginate(_getAccountStorage().executors, cursor, size);
     }
 
+    /// @notice Retrieves the currently active hook address.
+    /// @return hook The address of the active hook module.
     function getActiveHook() external view returns (address hook) {
         return _getHook();
     }
 
+    /// @notice Fetches the fallback handler for a specific selector.
+    /// @param selector The function selector to query.
+    /// @return calltype The type of call that the handler manages.
+    /// @return handler The address of the fallback handler.
     function getFallbackHandlerBySelector(bytes4 selector) external view returns (CallType, address) {
         FallbackHandler memory handler = _getAccountStorage().fallbacks[selector];
         return (handler.calltype, handler.handler);
     }
 
+    /// @dev Initializes the module manager by setting up default states for validators and executors.
     function _initModuleManager() internal virtual {
         // account module storage
         AccountStorage storage ams = _getAccountStorage();
@@ -168,8 +186,9 @@ abstract contract ModuleManager is Storage, Receiver {
         ams.validators.init();
     }
 
-    // // TODO
-    // // Review this agaisnt required hook/permissions at the time of installations
+    /// @dev Installs a new validator module after checking if it matches the required module type.
+    /// @param validator The address of the validator module to be installed.
+    /// @param data Initialization data to configure the validator upon installation.
     function _installValidator(address validator, bytes calldata data) internal virtual {
         // Note: Idea is should be able to check supported interface and module type - eligible validator
         if (!IModule(validator).isModuleType(MODULE_TYPE_VALIDATOR)) revert IncompatibleValidatorModule(validator);
@@ -179,9 +198,13 @@ abstract contract ModuleManager is Storage, Receiver {
         IValidator(validator).onInstall(data);
     }
 
+    /// @dev Uninstalls a validator module /!\ ensuring the account retains at least one validator.
+    /// @param validator The address of the validator to be uninstalled.
+    /// @param data De-initialization data to configure the validator upon uninstallation.
     function _uninstallValidator(address validator, bytes calldata data) internal virtual {
-        // check if its the last validator. this might brick the account
-        (address[] memory array, ) = _getValidatorsPaginated(address(0x1), 2);
+        // Check if the account has at least one validator installed before proceeding
+        // Having at least one validator is a requirement for the account to function properly
+        (address[] memory array, ) = _paginate(_getAccountStorage().validators, address(0x1), 2);
         if (array.length == 1) {
             revert CannotRemoveLastValidator();
         }
@@ -197,6 +220,9 @@ abstract contract ModuleManager is Storage, Receiver {
     // //  Manage Executors
     // ////////////////////////////////////////////////////
 
+    /// @dev Installs a new executor module after checking if it matches the required module type.
+    /// @param executor The address of the executor module to be installed.
+    /// @param data Initialization data to configure the executor upon installation.
     function _installExecutor(address executor, bytes calldata data) internal virtual {
         // Note: Idea is should be able to check supported interface and module type - eligible validator
         if (!IModule(executor).isModuleType(MODULE_TYPE_EXECUTOR)) revert IncompatibleExecutorModule(executor);
@@ -206,6 +232,9 @@ abstract contract ModuleManager is Storage, Receiver {
         IExecutor(executor).onInstall(data);
     }
 
+    /// @dev Uninstalls an executor module by removing it from the executors list.
+    /// @param executor The address of the executor to be uninstalled.
+    /// @param data De-initialization data to configure the executor upon uninstallation.
     function _uninstallExecutor(address executor, bytes calldata data) internal virtual {
         SentinelListLib.SentinelList storage executors = _getAccountStorage().executors;
         (address prev, bytes memory disableModuleData) = abi.decode(data, (address, bytes));
@@ -217,6 +246,9 @@ abstract contract ModuleManager is Storage, Receiver {
     // //  Manage Hook
     // ////////////////////////////////////////////////////
 
+    /// @dev Installs a hook module, ensuring no other hooks are installed before proceeding.
+    /// @param hook The address of the hook to be installed.
+    /// @param data Initialization data to configure the hook upon installation.
     function _installHook(address hook, bytes calldata data) internal virtual {
         address currentHook = _getHook();
         if (currentHook != address(0)) {
@@ -227,15 +259,23 @@ abstract contract ModuleManager is Storage, Receiver {
         IHook(hook).onInstall(data);
     }
 
+    /// @dev Uninstalls a hook module, ensuring the current hook matches the one intended for uninstallation.
+    /// @param hook The address of the hook to be uninstalled.
+    /// @param data De-initialization data to configure the hook upon uninstallation.
     function _uninstallHook(address hook, bytes calldata data) internal virtual {
         _setHook(address(0));
         IHook(hook).onUninstall(data);
     }
 
+    /// @dev Sets the current hook in the storage to the specified address.
+    /// @param hook The new hook address.
     function _setHook(address hook) internal virtual {
         _getAccountStorage().hook = IHook(hook);
     }
 
+    /// @dev Installs a fallback handler for a given selector with initialization data.
+    /// @param handler The address of the fallback handler to install.
+    /// @param params The initialization parameters including the selector and call type.
     function _installFallbackHandler(address handler, bytes calldata params) internal virtual {
         bytes4 selector = bytes4(params[0:4]);
         CallType calltype = CallType.wrap(bytes1(params[4]));
@@ -245,6 +285,9 @@ abstract contract ModuleManager is Storage, Receiver {
         IFallback(handler).onInstall(initData);
     }
 
+    /// @dev Uninstalls a fallback handler for a given selector.
+    /// @param fallbackHandler The address of the fallback handler to uninstall.
+    /// @param data The de-initialization data containing the selector.
     function _uninstallFallbackHandler(address fallbackHandler, bytes calldata data) internal virtual {
         bytes4 selector = bytes4(data[0:4]);
         bytes memory deInitData = data[4:];
@@ -253,30 +296,48 @@ abstract contract ModuleManager is Storage, Receiver {
         IFallback(fallbackHandler).onUninstall(deInitData);
     }
 
+    /// @dev Checks if a fallback handler is set for a given selector.
+    /// @param selector The function selector to check.
+    /// @return True if a fallback handler is set, otherwise false.
     function _isFallbackHandlerInstalled(bytes4 selector) internal view virtual returns (bool) {
         FallbackHandler storage handler = _getAccountStorage().fallbacks[selector];
         return handler.handler != address(0);
     }
 
+    /// @dev Checks if the expected fallback handler is installed for a given selector.
+    /// @param selector The function selector to check.
+    /// @param expectedHandler The address of the handler expected to be installed.
+    /// @return True if the installed handler matches the expected handler, otherwise false.
     function _isFallbackHandlerInstalled(bytes4 selector, address expectedHandler) internal view returns (bool) {
         FallbackHandler storage handler = _getAccountStorage().fallbacks[selector];
         return handler.handler == expectedHandler;
     }
 
+    /// @dev Checks if a validator is currently installed.
+    /// @param validator The address of the validator to check.
+    /// @return True if the validator is installed, otherwise false.
     function _isValidatorInstalled(address validator) internal view virtual returns (bool) {
         SentinelListLib.SentinelList storage validators = _getAccountStorage().validators;
         return validators.contains(validator);
     }
 
+    /// @dev Checks if an executor is currently installed.
+    /// @param executor The address of the executor to check.
+    /// @return True if the executor is installed, otherwise false.
     function _isExecutorInstalled(address executor) internal view virtual returns (bool) {
         SentinelListLib.SentinelList storage executors = _getAccountStorage().executors;
         return executors.contains(executor);
     }
 
+    /// @dev Checks if a hook is currently installed.
+    /// @param hook The address of the hook to check.
+    /// @return True if the hook is installed, otherwise false.
     function _isHookInstalled(address hook) internal view returns (bool) {
         return _getHook() == hook;
     }
 
+    /// @dev Retrieves the current hook from the storage.
+    /// @return hook The address of the current hook.
     function _getHook() internal view returns (address hook) {
         hook = address(_getAccountStorage().hook);
     }
