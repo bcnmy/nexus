5736fce8021698559d8ffd9c0d01e01f6f1366b4
aboudjem <boudjemaa.adam@gmail.com>
2024-04-23 13:27:40 +0400

    🖼️ Add natspec on BaseAccount

--- contracts/base/BaseAccount.sol
+++ contracts/base/BaseAccount.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.24;
 
 import { IEntryPoint } from "account-abstraction/contracts/interfaces/IEntryPoint.sol";
+import { IBaseAccount } from "../interfaces/base/IBaseAccount.sol";
 
 // has ERC-4337-v-0.7 implementations
 // houses ERC7579 config implementations
@@ -13,34 +14,31 @@ import { IEntryPoint } from "account-abstraction/contracts/interfaces/IEntryPoin
  * @author zeroknots.eth | rhinestone.wtf, chirag@biconomy.io
  * shoutout to solady (vectorized, ross) for this code
  */
-contract BaseAccount {
-    error AccountAccessUnauthorized();
+contract BaseAccount is IBaseAccount {
 
+    /// @notice Identifier for this implementation on the network
     string internal constant _ACCOUNT_IMPLEMENTATION_ID = "biconomy.modular-smart-account.1.0.0-alpha";
+
+    /// @notice The canonical address for the ERC4337 EntryPoint contract, version 0.7.
+    /// This address is consistent across all supported networks.
     address private constant _ENTRYPOINT = 0x0000000071727De22E5E9d8BAf0edAc6f37da032;
 
-    /////////////////////////////////////////////////////
-    // Access Control
-    ////////////////////////////////////////////////////
-
-    modifier onlyEntryPointOrSelf() virtual {
-        if (!(msg.sender == _ENTRYPOINT || msg.sender == address(this))) {
+    /// @dev Ensures the caller is either the EntryPoint or this contract itself.
+    /// Reverts with AccountAccessUnauthorized if the check fails.
+    modifier onlyEntryPointOrSelf() {
+        if (msg.sender != _ENTRYPOINT && msg.sender != address(this)) {
             revert AccountAccessUnauthorized();
         }
         _;
     }
 
-    modifier onlyEntryPoint() virtual {
+    /// @dev Ensures the caller is the EntryPoint.
+    /// Reverts with AccountAccessUnauthorized if the check fails.
+    modifier onlyEntryPoint() {
         if (msg.sender != _ENTRYPOINT) {
             revert AccountAccessUnauthorized();
         }
         _;
-    }
-
-    /// @dev Returns the canonical ERC4337 EntryPoint contract.
-    /// Override this function to return a different EntryPoint.
-    function entryPoint() external pure returns (address) {
-        return _ENTRYPOINT;
     }
 
     /// @dev Sends to the EntryPoint (i.e. `msg.sender`) the missing funds for this transaction.
@@ -61,6 +59,18 @@ contract BaseAccount {
         }
     }
 
+    /// @notice Retrieves the address of the EntryPoint contract, currently using version 0.7.
+    /// @dev This function returns the address of the canonical ERC4337 EntryPoint contract.
+    /// It can be overridden to return a different EntryPoint address if needed.
+    /// @return The address of the EntryPoint contract.
+    function entryPoint() external pure returns (address) {
+        return _ENTRYPOINT;
+    }
+
+
+    /// @notice Gets the nonce for a particular key.
+    /// @param key The nonce key.
+    /// @return The nonce associated with the key.
     function nonce(uint192 key) external view virtual returns (uint256) {
         return IEntryPoint(_ENTRYPOINT).getNonce(address(this), key);
     }
@@ -69,7 +79,8 @@ contract BaseAccount {
     /*                     DEPOSIT OPERATIONS                     */
     /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
 
-    /// @dev Returns the account's balance on the EntryPoint.
+        /// @notice Returns the current deposit balance of this account on the EntryPoint.
+    /// @return result The current balance held at the EntryPoint.
     function getDeposit() external view virtual returns (uint256 result) {
         /// @solidity memory-safe-assembly
         assembly {
@@ -87,19 +98,20 @@ contract BaseAccount {
         }
     }
 
-    /// @dev Deposit more funds for this account in the EntryPoint.
+    /// @notice Adds deposit to the EntryPoint to fund transactions.
     function addDeposit() external payable virtual {
         /// @solidity memory-safe-assembly
         assembly {
             // The EntryPoint has balance accounting logic in the `receive()` function.
-            // forgefmt: disable-next-item
             if iszero(mul(extcodesize(_ENTRYPOINT), call(gas(), _ENTRYPOINT, callvalue(), codesize(), 0x00, codesize(), 0x00))) {
                 revert(codesize(), 0x00) // For gas estimation.
             }
         }
     }
 
-    /// @dev Withdraw ETH from the account's deposit on the EntryPoint.
+    /// @notice Withdraws ETH from the EntryPoint to a specified address.
+    /// @param to The address to receive the withdrawn funds.
+    /// @param amount The amount to withdraw.
     function withdrawDepositTo(address to, uint256 amount) external payable virtual onlyEntryPointOrSelf {
         /// @solidity memory-safe-assembly
         assembly {
