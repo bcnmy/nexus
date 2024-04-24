0e66e6aa0fbdb003ba17b042228448dc4eeea860
aboudjem <boudjemaa.adam@gmail.com>
2024-04-24 20:23:32 +0400

    Refactor BaseAccount contract and update documentation

--- contracts/base/BaseAccount.sol
+++ contracts/base/BaseAccount.sol
@@ -1,22 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+// ──────────────────────────────────────────────────────────────────────────────
+//     _   __    _  __
+//    / | / /__ | |/ /_  _______
+//   /  |/ / _ \|   / / / / ___/
+//  / /|  /  __/   / /_/ (__  )
+// /_/ |_/\___/_/|_\__,_/____/
+//
+// ──────────────────────────────────────────────────────────────────────────────
+// Nexus: A suite of contracts for Modular Smart Accounts compliant with ERC-7579 and ERC-4337,
+// using Entrypoint version 0.7.0, developed by Biconomy. Learn more at https://biconomy.io/
+
 import { IEntryPoint } from "account-abstraction/contracts/interfaces/IEntryPoint.sol";
 import { IBaseAccount } from "../interfaces/base/IBaseAccount.sol";
 
-// has ERC-4337-v-0.7 implementations
-// houses ERC7579 config implementations
-// houses access control
-
-/**
- * @title BaseAccount
- * @dev Base contract housing methods specific to ERC4337 and some of ERC7579 configuration
- * @author zeroknots.eth | rhinestone.wtf, chirag@biconomy.io
- * shoutout to solady (vectorized, ross) for this code
- */
+/// @title Nexus - BaseAccount
+/// @notice Implements ERC-4337 and ERC-7579 standards for account management and access control within the Nexus suite.
+/// @dev Manages entry points and configurations as specified in the ERC-4337 and ERC-7579 documentation.
+/// @author @livingrockrises | Biconomy | chirag@biconomy.io
+/// @author @aboudjem | Biconomy | adam.boudjemaa@biconomy.io
+/// @author @filmakarov | Biconomy | filipp.makarov@biconomy.io
+/// @author @zeroknots | Rhinestone.wtf | zeroknots.eth
+/// Special thanks to the Solady team for foundational contributions: https://github.com/Vectorized/solady
 contract BaseAccount is IBaseAccount {
     /// @notice Identifier for this implementation on the network
-    string internal constant _ACCOUNT_IMPLEMENTATION_ID = "biconomy.modular-smart-account.1.0.0-alpha";
+    string internal constant _ACCOUNT_IMPLEMENTATION_ID = "biconomy.nexus.1.0.0";
 
     /// @notice The canonical address for the ERC4337 EntryPoint contract, version 0.7.
     /// This address is consistent across all supported networks.
@@ -58,12 +67,32 @@ contract BaseAccount is IBaseAccount {
         }
     }
 
-    /// @notice Retrieves the address of the EntryPoint contract, currently using version 0.7.
-    /// @dev This function returns the address of the canonical ERC4337 EntryPoint contract.
-    /// It can be overridden to return a different EntryPoint address if needed.
-    /// @return The address of the EntryPoint contract.
-    function entryPoint() external pure returns (address) {
-        return _ENTRYPOINT;
+    /// @notice Adds deposit to the EntryPoint to fund transactions.
+    function addDeposit() external payable virtual {
+        /// @solidity memory-safe-assembly
+        assembly {
+            // The EntryPoint has balance accounting logic in the `receive()` function.
+            if iszero(mul(extcodesize(_ENTRYPOINT), call(gas(), _ENTRYPOINT, callvalue(), codesize(), 0x00, codesize(), 0x00))) {
+                revert(codesize(), 0x00)
+            } // For gas estimation.
+        }
+    }
+
+    /// @notice Withdraws ETH from the EntryPoint to a specified address.
+    /// @param to The address to receive the withdrawn funds.
+    /// @param amount The amount to withdraw.
+    function withdrawDepositTo(address to, uint256 amount) external payable virtual onlyEntryPointOrSelf {
+        /// @solidity memory-safe-assembly
+        assembly {
+            mstore(0x14, to) // Store the `to` argument.
+            mstore(0x34, amount) // Store the `amount` argument.
+            mstore(0x00, 0x205c2878000000000000000000000000) // `withdrawTo(address,uint256)`.
+            if iszero(mul(extcodesize(_ENTRYPOINT), call(gas(), _ENTRYPOINT, 0, 0x10, 0x44, codesize(), 0x00))) {
+                returndatacopy(mload(0x40), 0x00, returndatasize())
+                revert(mload(0x40), returndatasize())
+            }
+            mstore(0x34, 0) // Restore the part of the free memory pointer that was overwritten.
+        }
     }
 
     /// @notice Gets the nonce for a particular key.
@@ -72,10 +101,6 @@ contract BaseAccount is IBaseAccount {
     function nonce(uint192 key) external view virtual returns (uint256) {
         return IEntryPoint(_ENTRYPOINT).getNonce(address(this), key);
     }
-
-    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
-    /*                     DEPOSIT OPERATIONS                     */
-    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
 
     /// @notice Returns the current deposit balance of this account on the EntryPoint.
     /// @return result The current balance held at the EntryPoint.
@@ -96,33 +121,11 @@ contract BaseAccount is IBaseAccount {
         }
     }
 
-    /// @notice Adds deposit to the EntryPoint to fund transactions.
-    function addDeposit() external payable virtual {
-        /// @solidity memory-safe-assembly
-        assembly {
-            // The EntryPoint has balance accounting logic in the `receive()` function.
-            if iszero(
-                mul(extcodesize(_ENTRYPOINT), call(gas(), _ENTRYPOINT, callvalue(), codesize(), 0x00, codesize(), 0x00))
-            ) {
-                revert(codesize(), 0x00) // For gas estimation.
-            }
-        }
-    }
-
-    /// @notice Withdraws ETH from the EntryPoint to a specified address.
-    /// @param to The address to receive the withdrawn funds.
-    /// @param amount The amount to withdraw.
-    function withdrawDepositTo(address to, uint256 amount) external payable virtual onlyEntryPointOrSelf {
-        /// @solidity memory-safe-assembly
-        assembly {
-            mstore(0x14, to) // Store the `to` argument.
-            mstore(0x34, amount) // Store the `amount` argument.
-            mstore(0x00, 0x205c2878000000000000000000000000) // `withdrawTo(address,uint256)`.
-            if iszero(mul(extcodesize(_ENTRYPOINT), call(gas(), _ENTRYPOINT, 0, 0x10, 0x44, codesize(), 0x00))) {
-                returndatacopy(mload(0x40), 0x00, returndatasize())
-                revert(mload(0x40), returndatasize())
-            }
-            mstore(0x34, 0) // Restore the part of the free memory pointer that was overwritten.
-        }
+    /// @notice Retrieves the address of the EntryPoint contract, currently using version 0.7.
+    /// @dev This function returns the address of the canonical ERC4337 EntryPoint contract.
+    /// It can be overridden to return a different EntryPoint address if needed.
+    /// @return The address of the EntryPoint contract.
+    function entryPoint() external pure returns (address) {
+        return _ENTRYPOINT;
     }
 }
