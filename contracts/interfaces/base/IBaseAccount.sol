a2c21e28becfd4e3d04d4cca0977ea3f3c2a42c8
aboudjem <boudjemaa.adam@gmail.com>
2024-04-24 20:25:01 +0400

    ✨ Update IBaseAccount interface with Nexus integration and documentation

--- contracts/interfaces/base/IBaseAccount.sol
+++ contracts/interfaces/base/IBaseAccount.sol
@@ -1,12 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-/**
- * @title BaseAccountData
- * @dev Interface for defining common data structures and errors for the BaseAccount.
- */
+// ──────────────────────────────────────────────────────────────────────────────
+//     _   __    _  __
+//    / | / /__ | |/ /_  _______
+//   /  |/ / _ \|   / / / / ___/
+//  / /|  /  __/   / /_/ (__  )
+// /_/ |_/\___/_/|_\__,_/____/
+//
+// ──────────────────────────────────────────────────────────────────────────────
+// Nexus: A suite of contracts for Modular Smart Accounts compliant with ERC-7579 and ERC-4337,
+// using Entrypoint version 0.7.0, developed by Biconomy. Learn more at https://biconomy.io/
+
+/// @title Nexus - IBaseAccount
+/// @notice Defines common data structures and errors for the BaseAccount within the Nexus suite.
+/// @dev Interface for organizing the base functionalities, events and errors.
+/// @author @livingrockrises | Biconomy | chirag@biconomy.io
+/// @author @aboudjem | Biconomy | adam.boudjemaa@biconomy.io
+/// @author @filmakarov | Biconomy | filipp.makarov@biconomy.io
+/// @author @zeroknots | Rhinestone.wtf | zeroknots.eth
+/// Special thanks to the Solady team for foundational contributions: h ttps://github.com/Vectorized/solady
 interface IBaseAccount {
-    
     /// @dev Emitted when an unauthorized access attempt occurs.
     error AccountAccessUnauthorized();
 }
