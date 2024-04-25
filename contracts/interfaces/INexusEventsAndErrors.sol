fcaf887dfb383ef4f15084e889b69979402349df
aboudjem <boudjemaa.adam@gmail.com>
2024-04-25 15:15:12 +0400

    Add INexusEventsAndErrors interface

---
+++ contracts/interfaces/INexusEventsAndErrors.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+// ──────────────────────────────────────────────────────────────────────────────
+//     _   __    _  __
+//    / | / /__ | |/ /_  _______
+//   /  |/ / _ \|   / / / / ___/
+//  / /|  /  __/   / /_/ (__  )
+// /_/ |_/\___/_/|_\__,_/____/
+//
+// ──────────────────────────────────────────────────────────────────────────────
+// Nexus: A suite of contracts for Modular Smart Accounts compliant with ERC-7579 and ERC-4337,
+// using Entrypoint version 0.7.0, developed by Biconomy. Learn more at https://biconomy.io/
+
+import { CallType, ExecType } from "../lib/ModeLib.sol";
+
+/// @title Nexus - INexus Events and Errors
+/// @notice Defines common errors for the Nexus smart account management interface.
+/// @author @livingrockrises | Biconomy | chirag@biconomy.io
+/// @author @aboudjem | Biconomy | adam.boudjemaa@biconomy.io
+/// @author @filmakarov | Biconomy | filipp.makarov@biconomy.io
+/// @author @zeroknots | Rhinestone.wtf | zeroknots.eth
+/// Special thanks to the Solady team for foundational contributions: https://github.com/Vectorized/solady
+interface INexusEventsAndErrors {
+    // Error thrown when an unsupported ModuleType is requested
+    error UnsupportedModuleType(uint256 moduleTypeId);
+
+    // Error thrown when an execution with an unsupported CallType was made
+    error UnsupportedCallType(CallType callType);
+
+    // Error thrown when an execution with an unsupported ExecType was made
+    error UnsupportedExecType(ExecType execType);
+
+    // Error thrown on failed execution
+    error ExecutionFailed();
+}
