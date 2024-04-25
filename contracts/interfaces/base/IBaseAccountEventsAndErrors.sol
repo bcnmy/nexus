1744b5daf115774dc0d58a07d6ea227fa3b73cd4
aboudjem <boudjemaa.adam@gmail.com>
2024-04-25 14:56:05 +0400

    ✨ Add IBaseAccountEventsAndErrors interface

---
+++ contracts/interfaces/base/IBaseAccountEventsAndErrors.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+// ──────────────────────────────────────────────────────────────────────────────
+//     _   __    _  __
+//    / | / /__ | |/ /_  _______
+//   /  |/ / _ \|   / / / / ___/
+//  / /|  /  __/   / /_/ (__  )
+// /_/ |_/\___/_/|_\__,_/____/
+//
+// ──────────────────────────────────────────────────────────────────────────────
+// Nexus: A suite of contracts for Modular Smart Accounts compliant with ERC-7579 and ERC-4337,
+// using Entrypoint version 0.7.0, developed by Biconomy. Learn more at https://biconomy.io/
+
+/// @title Execution Manager Events and Errors Interface
+/// @notice Interface for defining events and errors related to transaction execution processes within smart accounts.
+/// @dev This interface defines events and errors used by execution manager to handle and report the operational status of smart account transactions.
+/// It is a part of the Nexus suite of contracts aimed at implementing flexible and secure smart account operations.
+/// @author @livingrockrises | Biconomy | chirag@biconomy.io
+/// @author @aboudjem | Biconomy | adam.boudjemaa@biconomy.io
+/// @author @filmakarov | Biconomy | filipp.makarov@biconomy.io
+/// @author @zeroknots | Rhinestone.wtf | zeroknots.eth
+/// Special thanks to the Solady team for foundational contributions: https://github.com/Vectorized/solady
+interface IBaseAccountEventsAndErrors {
+    /// @dev Throws an error when a caller is not authorized to access an account.
+    error AccountAccessUnauthorized();
+}
