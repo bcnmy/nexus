7a8c6ce3a381003a649573108d80c97dadca4ed8
aboudjem <boudjemaa.adam@gmail.com>
2024-05-08 01:50:23 +0400

    test: add fuzz test for AccountFactory deployment

---
+++ test/foundry/unit/fuzz/TestFuzz_AccountFactory_Deployment.t.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "../../utils/Imports.sol";
+import "../../utils/SmartAccountTestLab.t.sol";
+
+contract TestFuzz_AccountFactory_Deployment is SmartAccountTestLab {
+    function setUp() public {
+        init();
+    }
+
+    function testFuzz_CreateAccountWithRandomData(uint256 randomSeed) public {
+        Vm.Wallet memory randomUser = createAndFundWallet("RandomUser", 1 ether);
+        bytes memory initData = abi.encodePacked(randomUser.addr, randomSeed);
+
+        address payable expectedAddress = FACTORY.getCounterFactualAddress(address(VALIDATOR_MODULE), initData, 0);
+        address payable deployedAccountAddress = FACTORY.createAccount(address(VALIDATOR_MODULE), initData, 0);
+
+        assertEq(deployedAccountAddress, expectedAddress, "Deployed account address should match expected address");
+    }
+}
