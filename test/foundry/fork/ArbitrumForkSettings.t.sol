a0b17b6e177867721770e8fa076bcb1d4f0325aa
aboudjem <boudjemaa.adam@gmail.com>
2024-05-07 22:02:52 +0400

    chore: Update ARBITRUM_RPC_URL constant in ArbitrumForkSettings contract

--- test/foundry/fork/ArbitrumForkSettings.t.sol
+++ test/foundry/fork/ArbitrumForkSettings.t.sol
@@ -7,5 +7,5 @@ contract ArbitrumForkSettings {
     address public constant ENTRYPOINT_ADDRESS = 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789;
     address public constant MODULE_ADDRESS = 0x000000824dc138db84FD9109fc154bdad332Aa8E;
     address public constant USDC_ADDRESS = 0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8;
-    string public constant ARBITRUM_RPC_URL = "https://arb1.arbitrum.io/rpc";
+    string public constant ARBITRUM_RPC_URL = "https://arbitrum-one-archive.allthatnode.com";
 }
