7a9f05d5733c8c6a4fb16669b4441ed26d0c477b
aboudjem <boudjemaa.adam@gmail.com>
2024-05-07 13:33:08 +0400

    chore: Add ArbitrumForkSettings contract with constants for arbitrum

---
+++ test/foundry/fork/ArbitrumForkSettings.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract ArbitrumForkSettings {
+    address public constant PROXY_ADDRESS = 0x64DB18E4428e710cd0332fFf2A079EA75D04E085;
+    address public constant OWNER_ADDRESS = 0x5862a48dB87d4FaD3Aa52498A3F25389ddefA2ee;
+    address public constant ENTRYPOINT_ADDRESS = 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789;
+    address public constant MODULE_ADDRESS = 0x000000824dc138db84FD9109fc154bdad332Aa8E;
+    address public constant USDC_ADDRESS = 0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8;
+    string public constant ARBITRUM_RPC_URL = "https://arb1.arbitrum.io/rpc";
+}
