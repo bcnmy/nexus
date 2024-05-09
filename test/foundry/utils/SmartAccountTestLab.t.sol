997a20c68dbbc219cf73b70479069b8f1da70641
aboudjem <boudjemaa.adam@gmail.com>
2024-05-09 19:56:17 +0400

    chore: Refactor prepareUserOperation to preparePackedUserOperation for consistency

--- test/foundry/utils/SmartAccountTestLab.t.sol
+++ test/foundry/utils/SmartAccountTestLab.t.sol
@@ -13,31 +13,16 @@ contract SmartAccountTestLab is Helpers {
     }
 
     function _prefundSmartAccountAndAssertSuccess(address sa, uint256 prefundAmount) internal {
-        (bool res,) = sa.call{ value: prefundAmount }(""); // Pre-funding the account contract
+        (bool res, ) = sa.call{ value: prefundAmount }(""); // Pre-funding the account contract
         assertTrue(res, "Pre-funding account should succeed");
     }
 
-    function _prepareSingleExecution(
-        address to,
-        uint256 value,
-        bytes memory data
-    )
-        internal
-        pure
-        returns (Execution[] memory execution)
-    {
+    function _prepareSingleExecution(address to, uint256 value, bytes memory data) internal pure returns (Execution[] memory execution) {
         execution = new Execution[](1);
         execution[0] = Execution(to, value, data);
     }
 
-    function _prepareSeveralIdenticalExecutions(
-        Execution memory execution,
-        uint256 executionsNumber
-    )
-        internal
-        pure
-        returns (Execution[] memory)
-    {
+    function _prepareSeveralIdenticalExecutions(Execution memory execution, uint256 executionsNumber) internal pure returns (Execution[] memory) {
         Execution[] memory executions = new Execution[](executionsNumber);
         for (uint256 i = 0; i < executionsNumber; i++) {
             executions[i] = execution;
@@ -45,17 +30,11 @@ contract SmartAccountTestLab is Helpers {
         return executions;
     }
 
-    function handleUserOpAndMeasureGas(
-        PackedUserOperation[] memory userOps,
-        address refundReceiver
-    )
-        internal
-        returns (uint256 gasUsed)
-    {
+    function handleUserOpAndMeasureGas(PackedUserOperation[] memory userOps, address refundReceiver) internal returns (uint256 gasUsed) {
         uint256 gasStart = gasleft();
         ENTRYPOINT.handleOps(userOps, payable(refundReceiver));
         gasUsed = gasStart - gasleft();
     }
 
-    receive() external payable { }
+    receive() external payable {}
 }
