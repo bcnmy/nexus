f6cee6e19a5b2f26fd513eae7c8a4a083a663d69
aboudjem <boudjemaa.adam@gmail.com>
2024-05-09 16:04:39 +0400

    Rename ProxyUpgradeProcessTests to smartAccountV2UpgradeProcessTests and update variable names

--- test/foundry/fork/ProxyUpgradeTests.t.sol
+++ test/foundry/fork/ProxyUpgradeTests.t.sol
@@ -8,8 +8,8 @@ import "../utils/SmartAccountTestLab.t.sol";
 import "../../../contracts/interfaces/base/IAccountConfig.sol";
 import "./TestInterfacesAndStructs.t.sol";
 
-contract ProxyUpgradeProcessTests is SmartAccountTestLab, ArbitrumForkSettings {
-    IProxyV2 public proxy;
+contract smartAccountV2UpgradeProcessTests is SmartAccountTestLab, ArbitrumForkSettings {
+    IBiconomySmartAccountV2 public smartAccountV2;
     IEntryPointV_0_6 public entryPoint;
     Nexus public newImplementation;
 
@@ -18,44 +18,44 @@ contract ProxyUpgradeProcessTests is SmartAccountTestLab, ArbitrumForkSettings {
         vm.selectFork(mainnetFork);
         vm.rollFork(200000000);
         init();
-        proxy = IProxyV2(PROXY_ADDRESS);
+        smartAccountV2 = IBiconomySmartAccountV2(SMART_ACCOUNT_V2_ADDRESS);
         entryPoint = IEntryPointV_0_6(ENTRYPOINT_ADDRESS);
         newImplementation = new Nexus();
     }
 
-    function test_UpgradeAndInitialize() public {
+    function test_UpgradeV2ToV3AndInitialize() public {
         // Check initial state before upgrade
-        address initialEntryPoint = Nexus(payable(address(proxy))).entryPoint();
-        assertEq(initialEntryPoint, ENTRYPOINT_ADDRESS, "Initial entry point mismatch.");
+        IEntryPointV_0_6 initialEntryPoint = smartAccountV2.entryPoint();
+        assertEq(address(initialEntryPoint), ENTRYPOINT_ADDRESS, "Initial entry point mismatch.");
 
-        vm.startPrank(PROXY_ADDRESS);
-        proxy.updateImplementation(address(newImplementation));
+        vm.startPrank(SMART_ACCOUNT_V2_ADDRESS);
+        smartAccountV2.updateImplementation(address(newImplementation));
 
         // Initialize the new implementation with the required validator module
         bytes memory initData = abi.encodePacked(BOB.addr);
-        Nexus(payable(address(proxy))).initialize(address(VALIDATOR_MODULE), initData);
+        Nexus(payable(address(smartAccountV2))).initialize(address(VALIDATOR_MODULE), initData);
 
         vm.stopPrank();
 
         // Check state after upgrade
-        address newEntryPoint = Nexus(payable(address(proxy))).entryPoint();
-        assertEq(newEntryPoint, address(ENTRYPOINT), "Entry point should not change after upgrade.");
+        address newEntryPoint = Nexus(payable(address(smartAccountV2))).entryPoint();
+        assertEq(newEntryPoint, address(ENTRYPOINT), "Entry point should change after upgrade.");
     }
 
 function test_AccountIdValidationAfterUpgrade() public {
-    test_UpgradeAndInitialize();  // Ensure the proxy is upgraded and initialized
+    test_UpgradeV2ToV3AndInitialize();  // Ensure the smartAccountV2 is upgraded and initialized
 
     string memory expectedAccountId = "biconomy.nexus.0.0.1";
-    string memory actualAccountId = IAccountConfig(payable(address(proxy))).accountId();
+    string memory actualAccountId = IAccountConfig(payable(address(smartAccountV2))).accountId();
     assertEq(actualAccountId, expectedAccountId, "Account ID does not match after upgrade.");
 }
 
     function test_USDCTransferPostUpgrade() public {
-        test_UpgradeAndInitialize();  // Ensure the setup and upgrade are complete
+        test_UpgradeV2ToV3AndInitialize();  // Ensure the setup and upgrade are complete
 
         MockToken usdc = MockToken(USDC_ADDRESS);
         address recipient = address(0x123);  // Random recipient address
-        uint256 amount = usdc.balanceOf(address(proxy));  // Full balance for transfer
+        uint256 amount = usdc.balanceOf(address(smartAccountV2));  // Full balance for transfer
 
         // Encode the call to USDC's transfer function
         bytes memory callData = abi.encodeWithSelector(usdc.transfer.selector, recipient, amount);
@@ -65,7 +65,7 @@ function test_AccountIdValidationAfterUpgrade() public {
         execution[0] = Execution(address(usdc), 0, callData);
 
         // Pack user operation
-        PackedUserOperation[] memory userOps = prepareUserOperation(BOB, Nexus(payable(address(PROXY_ADDRESS))), EXECTYPE_DEFAULT, execution);
+        PackedUserOperation[] memory userOps = prepareUserOperation(BOB, Nexus(payable(address(SMART_ACCOUNT_V2_ADDRESS))), EXECTYPE_DEFAULT, execution);
 
         // Execute the operation via the EntryPoint
         ENTRYPOINT.handleOps(userOps, payable(OWNER_ADDRESS));
