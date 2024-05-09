ab81ce0c80d9dce98df9ba46106e367050ddc35a
aboudjem <boudjemaa.adam@gmail.com>
2024-05-09 19:54:06 +0400

    chore: Update IBiconomySmartAccountV2 interface and UserOperation struct

--- test/foundry/fork/TestInterfacesAndStructs.t.sol
+++ test/foundry/fork/TestInterfacesAndStructs.t.sol
@@ -18,10 +18,13 @@ struct UserOperation {
 
 interface IBiconomySmartAccountV2 {
     function updateImplementation(address _implementation) external;
-    function entryPoint() external view returns (IEntryPointV_0_6);
+    function executeBatch(address[] calldata dest, uint256[] calldata value, bytes[] calldata func) external;
+    function entryPoint() external view returns (address);
 }
 
 interface IEntryPointV_0_6 {
     function handleOps(UserOperation[] calldata ops, address sender) external payable;
+    function depositTo(address account) external payable;
     function getUserOpHash(UserOperation calldata userOp) external view returns (bytes32);
+    function getNonce(address sender, uint192 key) external view returns (uint256 nonce);
 }
