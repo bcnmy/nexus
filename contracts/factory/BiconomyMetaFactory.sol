a8f4816ac19ba0677f2f0132416696c94d632388
livingrockrises <90545960+livingrockrises@users.noreply.github.com>
2024-05-16 02:51:57 +0400

    add meta factory

--- contracts/factory/BiconomyMetaFactory.sol
+++ contracts/factory/BiconomyMetaFactory.sol
@@ -13,10 +13,37 @@ pragma solidity ^0.8.24;
 // Learn more at https://biconomy.io. For security issues, contact: security@biconomy.io
 
 import { IAccountFactory } from "../interfaces/factory/IAccountFactory.sol";
+import { Stakeable } from "../common/Stakeable.sol";
 
 // can stake
 // can whitelist factories
 // deployAccount with chosen factory and required data for that facotry
-contract BiconomyMetaFactory {
+contract BiconomyMetaFactory is Stakeable {
 
+    error FactoryNotWhotelisted();
+
+    mapping(address => bool) public factoryWhitelist;
+
+    constructor(address owner) Stakeable(owner) {
+    }
+
+    // could be IAccountFactory
+    // whitelist / blacklist a factory
+    function whitelistFactory(address factory, bool whitelisted) external payable onlyOwner {
+        factoryWhitelist[factory] = whitelisted;
+    }
+
+    // Note: deploy using only one of the whitelisted factories
+    // these factories could possibly enshrine specific module/s
+    function deployWithFactory(IAccountFactory factory, bytes calldata initData, bytes32 salt)
+        external
+        payable
+        returns (address)
+    {
+        if (!factoryWhitelist[address(factory)]) {
+            revert FactoryNotWhotelisted();
+        }
+        // emit AccountDeployedWithFactory(factory, initData, salt);
+        return factory.createAccount(initData, salt);
+    }
 }
