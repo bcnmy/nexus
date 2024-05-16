c16674a7e3f3de841599a722e1b6b1ecde19f6e3
aboudjem <boudjemaa.adam@gmail.com>
2024-05-16 16:40:17 +0400

    refactor: add InvalidSignature error to EventsAndErrors.sol

--- test/foundry/utils/EventsAndErrors.sol
+++ test/foundry/utils/EventsAndErrors.sol
@@ -35,7 +35,8 @@ contract EventsAndErrors {
     error HookPostCheckFailed();
     error HookAlreadyInstalled(address currentHook);
     error FallbackAlreadyInstalledForSelector(bytes4 selector);
-    
+    error InvalidSignature();
+
     event TryExecuteUnsuccessful(uint256 batchExecutionindex, bytes result);
 
     error ERC1271InvalidSigner(address signer);
