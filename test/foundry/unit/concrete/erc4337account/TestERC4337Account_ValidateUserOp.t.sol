6a0ff5d8a3170d5783114874df0c4e4094014a23
Adam Boudj <boudjemaa.adam@gmail.com>
2024-05-17 15:10:47 +0400

    Feat/improve fix tests (#75)
    * add tests validate user operation from entry point
    
    * add accesscontrol+unauthorized access tests
    
    * refactor: add InvalidSignature error to EventsAndErrors.sol
    
    * add tests cases + fix wrong asserts
    
    * chore: Update deployment account funding amount in test
    
    * Remove unnecessary code in TestERC4337Account_ValidateUserOp.t.sol
    
    * add test for validation of user operation nonce
    
    * Merge branch 'dev' of https://github.com/bcnmy/erc7579-modular-smart-account into dev
    
    * ✏️ add comments on tests (validateUserOp)

--- test/foundry/unit/concrete/erc4337account/TestERC4337Account_ValidateUserOp.t.sol
+++ test/foundry/unit/concrete/erc4337account/TestERC4337Account_ValidateUserOp.t.sol
@@ -20,8 +20,9 @@ contract TestERC4337Account_ValidateUserOp is Test, SmartAccountTestLab {
         userOps[0].signature = signMessage(BOB, userOpHash);
 
         startPrank(address(ENTRYPOINT));
-        // Attempt to validate the user operation, expecting success
-        uint256 res = BOB_ACCOUNT.validateUserOp(userOps[0], userOpHash, 10);
+
+        // Attempt to validate the user operation, expecting success (return value of 0)
+        uint256 res = BOB_ACCOUNT.validateUserOp(userOps[0], userOpHash, 0);
         assertTrue(res == 0, "Valid operation should pass validation");
         stopPrank();
     }
@@ -31,12 +32,56 @@ contract TestERC4337Account_ValidateUserOp is Test, SmartAccountTestLab {
         PackedUserOperation[] memory userOps = new PackedUserOperation[](1);
         userOps[0] = buildPackedUserOp(userAddress, getNonce(address(BOB_ACCOUNT), address(VALIDATOR_MODULE)));
         bytes32 userOpHash = ENTRYPOINT.getUserOpHash(userOps[0]);
-        userOps[0].signature = signMessage(ALICE, userOpHash); // Incorrect signer simulated
+        // Sign the operation hash with ALICE's private key (incorrect signer)
+        userOps[0].signature = signMessage(ALICE, userOpHash);
 
         startPrank(address(ENTRYPOINT));
         // Attempt to validate the user operation, expecting failure due to invalid signature
         uint256 res = BOB_ACCOUNT.validateUserOp(userOps[0], userOpHash, 0);
         assertTrue(res == 1, "Operation with invalid signature should fail validation");
         stopPrank();
+    }
+
+    function test_ValidateUserOp_InvalidSignatureFormat() public {
+        PackedUserOperation[] memory userOps = new PackedUserOperation[](1);
+        userOps[0] = buildPackedUserOp(userAddress, getNonce(address(BOB_ACCOUNT), address(VALIDATOR_MODULE)));
+        bytes32 userOpHash = ENTRYPOINT.getUserOpHash(userOps[0]);
+        userOps[0].signature = "0x1234"; // Incorrect format, too short
+
+        startPrank(address(ENTRYPOINT));
+        // Expect the validation to revert due to invalid signature format
+
+        vm.expectRevert(InvalidSignature.selector);
+        BOB_ACCOUNT.validateUserOp(userOps[0], userOpHash, 0);
+        stopPrank();
+    }
+
+    function test_ValidateUserOp_InsufficientFunds() public {
+        PackedUserOperation[] memory userOps = new PackedUserOperation[](1);
+        userOps[0] = buildPackedUserOp(userAddress, getNonce(address(BOB_ACCOUNT), address(VALIDATOR_MODULE)));
+        bytes32 userOpHash = ENTRYPOINT.getUserOpHash(userOps[0]);
+        userOps[0].signature = signMessage(BOB, userOpHash);
+
+        startPrank(address(ENTRYPOINT));
+        // Attempt to validate the user operation, expecting failure due to insufficient funds
+
+        BOB_ACCOUNT.validateUserOp(userOps[0], userOpHash, 0.5 ether);
+        stopPrank();
+    }
+
+    function test_ValidateUserOp_InvalidNonce() public {
+        PackedUserOperation[] memory userOps = new PackedUserOperation[](1);
+        uint256 incorrectNonce = 123; // deliberately incorrect
+        userOps[0] = buildPackedUserOp(userAddress, incorrectNonce);
+        bytes32 userOpHash = ENTRYPOINT.getUserOpHash(userOps[0]);
+        userOps[0].signature = signMessage(BOB, userOpHash);
+
+        startPrank(address(ENTRYPOINT));
+
+        // Attempt to validate the user operation, expecting return value of 1 (failure)
+        uint res = BOB_ACCOUNT.validateUserOp(userOps[0], userOpHash, 0);
+        stopPrank();
+
+        assertTrue(res == 1, "Operation with invalid nonce should fail validation");
     }
 }
