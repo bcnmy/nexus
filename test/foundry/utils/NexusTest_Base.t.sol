e9f833747308f129addf232fb678d16872e3247a
aboudjem <boudjemaa.adam@gmail.com>
2024-05-18 17:57:58 +0400

    Refactor NexusTest_Base contract and remove unused code

--- test/foundry/utils/NexusTest_Base.t.sol
+++ test/foundry/utils/NexusTest_Base.t.sol
@@ -8,9 +8,6 @@ import "./EventsAndErrors.sol";
 /// @title NexusTest_Base - Base contract for testing Nexus smart account functionalities
 /// @notice This contract inherits from TestHelper to provide common setup and utilities for Nexus tests
 abstract contract NexusTest_Base is TestHelper {
-    Nexus public smartAccount;
-    Nexus public implementation;
-
     /// @notice Initializes the testing environment
     function init() internal virtual {
         setupTestEnvironment();
