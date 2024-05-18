fd72a878f31c7bba1858704f18425ea135d51b47
aboudjem <boudjemaa.adam@gmail.com>
2024-05-18 19:29:41 +0400

    Refactor code and add prefunding for smart account in TestFuzz_ValidateUserOp.t.sol

--- test/foundry/unit/fuzz/TestFuzz_ValidateUserOp.t.sol
+++ test/foundry/unit/fuzz/TestFuzz_ValidateUserOp.t.sol
@@ -17,6 +17,7 @@ contract TestFuzz_ValidateUserOp is NexusTest_Base {
     function testFuzz_ValidateUserOp_Valid(uint256 randomNonce) public {
         vm.assume(randomNonce < type(uint192).max);
 
+        // Prefund the smart account with sufficient funds
         prefundSmartAccountAndAssertSuccess(address(BOB_ACCOUNT), 1 ether);
 
         PackedUserOperation[] memory userOps = new PackedUserOperation[](1);
@@ -45,7 +46,7 @@ contract TestFuzz_ValidateUserOp is NexusTest_Base {
         PackedUserOperation[] memory userOps = new PackedUserOperation[](1);
         userOps[0] = buildUserOpWithCalldata(BOB, "", address(VALIDATOR_MODULE));
         bytes32 userOpHash = ENTRYPOINT.getUserOpHash(userOps[0]);
-        userOps[0].signature = signUserOp(ALICE, userOps[0]); // Using fuzzed signature
+        userOps[0].signature = signUserOp(ALICE, userOps[0]); // Using a signature from a different user
 
         // Attempt to validate the user operation
         startPrank(address(ENTRYPOINT));
