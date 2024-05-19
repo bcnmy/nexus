38eb27fc27e343123b3bda8b814b2ec0641225d5
aboudjem <boudjemaa.adam@gmail.com>
2024-05-19 23:43:04 +0400

    refactor: Improve comments and update natspec in TestAccountConfig_SupportsExecutionMode.t.sol

--- test/foundry/unit/concrete/accountconfig/TestAccountConfig_SupportsExecutionMode.t.sol
+++ test/foundry/unit/concrete/accountconfig/TestAccountConfig_SupportsExecutionMode.t.sol
@@ -4,28 +4,30 @@ pragma solidity ^0.8.24;
 import "../../../utils/Imports.sol";
 import "../../../utils/NexusTest_Base.t.sol";
 
-contract TestAccountConfig_SupportsExecutionMode is Test, NexusTest_Base {
+/// @title Test suite for checking execution modes supported by AccountConfig
+contract TestAccountConfig_SupportsExecutionMode is NexusTest_Base {
     Nexus public accountConfig;
 
+    /// @notice Initialize the testing environment
     function setUp() public {
         init();
         accountConfig = Nexus(BOB_ACCOUNT);
     }
 
+    /// @notice Tests if batch execution mode is supported
     function test_SupportsBatchExecutionMode() public {
         ExecutionMode mode = ModeLib.encodeSimpleBatch();
         assertTrue(accountConfig.supportsExecutionMode(mode), "AccountConfig should support batch execution mode.");
     }
 
+    /// @notice Tests if single execution mode is supported
     function test_SupportsSingleExecutionMode() public {
         ExecutionMode mode = ModeLib.encodeSimpleSingle();
         assertTrue(accountConfig.supportsExecutionMode(mode), "AccountConfig should support single execution mode.");
     }
 
-    // Optionally test for unsupported execution modes if any
-    // For example, if delegate calls are not supported
+    /// @notice Tests an unsupported execution mode
     function test_UnsupportedExecutionMode() public {
-        // Delegate calls are not supported, and using an arbitrary ExecutionMode for demonstration
         ExecutionMode unsupportedMode = ModeLib.encode(
             CALLTYPE_SINGLE,
             ExecType.wrap(0x10),
