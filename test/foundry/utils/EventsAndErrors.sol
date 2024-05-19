52b403964f188b3f943f5aac0a1b04647671c376
aboudjem <boudjemaa.adam@gmail.com>
2024-05-19 23:47:55 +0400

    refactor: improve and categorize events and errors

--- test/foundry/utils/EventsAndErrors.sol
+++ test/foundry/utils/EventsAndErrors.sol
@@ -5,40 +5,45 @@ contract EventsAndErrors {
     // Define all events
     event AccountCreated(address indexed account, address indexed validationModule, bytes moduleInstallData);
     event GenericFallbackCalled(address sender, uint256 value, bytes data);
-event Deposited(address indexed account, uint256 totalDeposit);
-
-
-event ModuleInstalled(uint256 moduleTypeId, address module);
-event ModuleUninstalled(uint256 moduleTypeId, address module);
-event UserOperationRevertReason(bytes32 indexed userOpHash, address indexed sender, uint256 nonce, bytes revertReason);
-
+    event Deposited(address indexed account, uint256 totalDeposit);
+    event ModuleInstalled(uint256 moduleTypeId, address module);
+    event ModuleUninstalled(uint256 moduleTypeId, address module);
+    event UserOperationRevertReason(bytes32 indexed userOpHash, address indexed sender, uint256 nonce, bytes revertReason);
     event PreCheckCalled();
     event PostCheckCalled();
+    event TryExecuteUnsuccessful(uint256 batchExecutionindex, bytes result);
 
     // Define all errors
-    error FailedOp(uint256 opIndex, string reason);
+    // General Errors
     error AccountInitializationFailed();
     error AccountAccessUnauthorized();
     error ExecutionFailed();
     error AlreadyInitialized(address smartAccount);
     error NotInitialized(address smartAccount);
+    error UnauthorizedOperation(address operator);
+
+    // Operation Errors
+    error FailedOp(uint256 opIndex, string reason);
+    error ERC20InsufficientAllowance(address spender, uint256 allowance, uint256 needed);
+    error ERC1271InvalidSigner(address signer);
+    error InvalidSignature();
+
+    // Linked List Errors
     error LinkedList_AlreadyInitialized();
     error LinkedList_InvalidPage();
-    error ERC20InsufficientAllowance(address spender, uint256 allowance, uint256 needed);
 
+    // Module Errors
     error CannotRemoveLastValidator();
     error InvalidModule(address module);
     error InvalidModuleTypeId(uint256 moduleTypeId);
     error ModuleAlreadyInstalled(uint256 moduleTypeId, address module);
-    error UnauthorizedOperation(address operator);
     error ModuleNotInstalled(uint256 moduleTypeId, address module);
     error ModuleAddressCanNotBeZero();
+
+    // Hook Errors
     error HookPostCheckFailed();
     error HookAlreadyInstalled(address currentHook);
+
+    // Fallback Errors
     error FallbackAlreadyInstalledForSelector(bytes4 selector);
-    error InvalidSignature();
-
-    event TryExecuteUnsuccessful(uint256 batchExecutionindex, bytes result);
-
-    error ERC1271InvalidSigner(address signer);
 }
