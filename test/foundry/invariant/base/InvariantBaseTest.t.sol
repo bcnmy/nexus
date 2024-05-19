ef54a1618401e6152a8314ca59717c4072fd49d9
aboudjem <boudjemaa.adam@gmail.com>
2024-05-19 23:47:31 +0400

    refactor: Update Solidity version to 0.8.24 in multiple files

--- test/foundry/invariant/base/InvariantBaseTest.t.sol
+++ test/foundry/invariant/base/InvariantBaseTest.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.24;
 
 import "../../utils/NexusTest_Base.t.sol";
 import "../../../../contracts/mocks/Counter.sol";
--- test/foundry/invariant/handlers/AccountCreationHandlerTest.t.sol
+++ test/foundry/invariant/handlers/AccountCreationHandlerTest.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.24;
 
 import { InvariantBaseTest } from "../base/InvariantBaseTest.t.sol";
 import "../../utils/Imports.sol";
