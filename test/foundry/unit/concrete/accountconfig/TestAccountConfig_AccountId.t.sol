e03e91c5d0d4dd049e07c533970f2124dcb0705c
aboudjem <boudjemaa.adam@gmail.com>
2024-05-19 23:42:56 +0400

    refactor: Improve comments and update natspec in TestAccountConfig_AccountId.t.sol

--- test/foundry/unit/concrete/accountconfig/TestAccountConfig_AccountId.t.sol
+++ test/foundry/unit/concrete/accountconfig/TestAccountConfig_AccountId.t.sol
@@ -3,13 +3,16 @@ pragma solidity ^0.8.24;
 
 import "../../../utils/Imports.sol";
 
+/// @title Test suite for checking account ID in AccountConfig
 contract TestAccountConfig_AccountId is Test {
     Nexus public accountConfig;
 
+    /// @notice Initialize the testing environment
     function setUp() public {
         accountConfig = new Nexus();
     }
 
+    /// @notice Tests if the account ID returns the expected value
     function test_AccountId_ReturnsExpectedValue() public {
         string memory expected = "biconomy.nexus.0.0.1";
         assertEq(accountConfig.accountId(), expected, "AccountConfig should return the expected account ID.");
