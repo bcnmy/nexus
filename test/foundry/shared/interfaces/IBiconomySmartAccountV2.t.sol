6349609621d8a074138f17a3e83656eb7108d6a6
aboudjem <boudjemaa.adam@gmail.com>
2024-05-20 01:29:46 +0400

    refactor: Add getImplementation function to IBiconomySmartAccountV2 interface

--- test/foundry/shared/interfaces/IBiconomySmartAccountV2.t.sol
+++ test/foundry/shared/interfaces/IBiconomySmartAccountV2.t.sol
@@ -5,4 +5,5 @@ interface IBiconomySmartAccountV2 {
     function updateImplementation(address _implementation) external;
     function executeBatch(address[] calldata dest, uint256[] calldata value, bytes[] calldata func) external;
     function entryPoint() external view returns (address);
+    function getImplementation() external view returns (address _implementation);
 }
