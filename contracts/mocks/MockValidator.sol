ba421a797e23416ab6c20e48b4ba6e6c1c96e4f8
aboudjem <boudjemaa.adam@gmail.com>
2024-05-20 00:38:00 +0400

    add check on onUninstall hook

--- contracts/mocks/MockValidator.sol
+++ contracts/mocks/MockValidator.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.24;
 
 import { IModule } from "../interfaces/modules/IModule.sol";
 import { IValidator } from "../interfaces/modules/IValidator.sol";
+import { IModuleManager } from "../interfaces/base/IModuleManager.sol";
 import { VALIDATION_SUCCESS, VALIDATION_FAILED, MODULE_TYPE_VALIDATOR, ERC1271_MAGICVALUE, ERC1271_INVALID } from "../types/Constants.sol";
 import { PackedUserOperation } from "account-abstraction/contracts/interfaces/PackedUserOperation.sol";
 import { ECDSA } from "solady/src/utils/ECDSA.sol";
@@ -33,6 +34,7 @@ contract MockValidator is IValidator {
 
     function onUninstall(bytes calldata data) external {
         data;
+        require(!IModuleManager(msg.sender).isModuleInstalled(MODULE_TYPE_VALIDATOR, address(this), ""), "Validator is still installed");
         delete smartAccountOwners[msg.sender];
     }
 
