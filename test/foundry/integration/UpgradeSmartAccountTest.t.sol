5614329fd7632fd37432c08d36626100580584b0
aboudjem <boudjemaa.adam@gmail.com>
2024-05-20 13:25:54 +0400

    refactor: Improve UpgradeSmartAccountTest with additional tests

--- test/foundry/integration/UpgradeSmartAccountTest.t.sol
+++ test/foundry/integration/UpgradeSmartAccountTest.t.sol
@@ -9,27 +9,38 @@ contract UpgradeSmartAccountTest is NexusTest_Base {
     function setUp() public {
         init();
     }
-    /// @notice Tests the upgrade of the smart account implementation
-    function test_upgradeSmartAccount() public {
-        Nexus newSmartAccount = new Nexus();
-        bytes32 slot = ACCOUNT_IMPLEMENTATION.proxiableUUID();
-        
-        // Check if the slot matches the expected UUID
-        assertEq(slot, 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc, "Proxiable UUID mismatch");
 
+    /// @notice Tests that the proxiable UUID slot is correct
+    function test_proxiableUUIDSlot() public {
+        bytes32 slot = ACCOUNT_IMPLEMENTATION.proxiableUUID();
+        assertEq(slot, 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc, "Proxiable UUID mismatch");
+    }
+
+    /// @notice Tests that the current implementation address is correct
+    function test_currentImplementationAddress() public {
         address currentImplementation = BOB_ACCOUNT.getImplementation();
         assertEq(currentImplementation, address(ACCOUNT_IMPLEMENTATION), "Current implementation address mismatch");
+    }
 
+    /// @notice Tests the upgrade of the smart account implementation
+    function test_upgradeImplementation() public {
+        Nexus newSmartAccount = new Nexus();
         bytes memory callData = abi.encodeWithSelector(Nexus.upgradeToAndCall.selector, address(newSmartAccount), "");
 
         Execution[] memory execution = new Execution[](1);
         execution[0] = Execution(address(BOB_ACCOUNT), 0, callData);
 
         PackedUserOperation[] memory userOps = buildPackedUserOperation(BOB, BOB_ACCOUNT, EXECTYPE_DEFAULT, execution, address(VALIDATOR_MODULE));
-
         ENTRYPOINT.handleOps(userOps, payable(address(BOB.addr)));
 
         address newImplementation = BOB_ACCOUNT.getImplementation();
         assertEq(newImplementation, address(newSmartAccount), "New implementation address mismatch");
+    }
+
+    /// @notice Tests the entire upgrade process
+    function test_upgradeSmartAccount() public {
+        test_proxiableUUIDSlot();
+        test_currentImplementationAddress();
+        test_upgradeImplementation();
     }
 }
