b0f5f69bf24d6084a972768685d1b467fa28aed2
aboudjem <boudjemaa.adam@gmail.com>
2024-05-21 21:55:13 +0400

    refactor: Update BootstrapUtil.sol to comply with styleguide

--- contracts/utils/BootstrapUtil.sol
+++ contracts/utils/BootstrapUtil.sol
@@ -6,39 +6,18 @@ import { IModule } from "../interfaces/modules/IModule.sol";
 
 // Review: can make this a library?
 contract BootstrapUtil {
-    function _makeBootstrapConfig(
-        address module,
-        bytes memory data
-    )
-        public
-        pure
-        returns (BootstrapConfig memory config)
-    {
+    function _makeBootstrapConfig(address module, bytes memory data) public pure returns (BootstrapConfig memory config) {
         config.module = module;
         config.data = data;
     }
 
-    function makeBootstrapConfig(
-        address module,
-        bytes memory data
-    )
-        public
-        pure
-        returns (BootstrapConfig[] memory config)
-    {
+    function makeBootstrapConfig(address module, bytes memory data) public pure returns (BootstrapConfig[] memory config) {
         config = new BootstrapConfig[](1);
         config[0].module = module;
         config[0].data = data;
     }
 
-    function makeBootstrapConfig(
-        address[] memory modules,
-        bytes[] memory datas
-    )
-        public
-        pure
-        returns (BootstrapConfig[] memory configs)
-    {
+    function makeBootstrapConfig(address[] memory modules, bytes[] memory datas) public pure returns (BootstrapConfig[] memory configs) {
         configs = new BootstrapConfig[](modules.length);
 
         for (uint256 i; i < modules.length; i++) {
