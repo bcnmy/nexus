3bc5eae6bfa01cf7bb7060a06610dc7d52210d84
aboudjem <boudjemaa.adam@gmail.com>
2024-05-22 12:26:41 +0400

    Update Stakeable Natspec

--- contracts/common/Stakeable.sol
+++ contracts/common/Stakeable.sol
@@ -26,7 +26,7 @@ contract Stakeable is Ownable, IStakeable {
 
     /// @notice Stakes a certain amount of Ether on an EntryPoint.
     /// @dev The contract should have enough Ether to cover the stake.
-    /// @param epAddress Address of the EntryPoint where the stake is added.
+    /// @param epAddress The address of the EntryPoint where the stake is added.
     /// @param unstakeDelaySec The delay in seconds before the stake can be unlocked.
     function addStake(address epAddress, uint32 unstakeDelaySec) external payable onlyOwner {
         require(epAddress != address(0), "Invalid EP address");
@@ -35,7 +35,7 @@ contract Stakeable is Ownable, IStakeable {
 
     /// @notice Unlocks the stake on an EntryPoint.
     /// @dev This starts the unstaking delay after which funds can be withdrawn.
-    /// @param epAddress Address of the EntryPoint where the stake is unlocked.
+    /// @param epAddress The address of the EntryPoint from which the stake is to be unlocked.
     function unlockStake(address epAddress) external onlyOwner {
         require(epAddress != address(0), "Invalid EP address");
         IEntryPoint(epAddress).unlockStake();
@@ -43,8 +43,8 @@ contract Stakeable is Ownable, IStakeable {
 
     /// @notice Withdraws the stake from an EntryPoint to a specified address.
     /// @dev This can only be done after the unstaking delay has passed since the unlock.
-    /// @param epAddress Address of the EntryPoint where the stake is withdrawn from.
-    /// @param withdrawAddress Address to receive the withdrawn stake.
+    /// @param epAddress The address of the EntryPoint where the stake is withdrawn from.
+    /// @param withdrawAddress The address to receive the withdrawn stake.
     function withdrawStake(address epAddress, address payable withdrawAddress) external onlyOwner {
         require(epAddress != address(0), "Invalid EP address");
         IEntryPoint(epAddress).withdrawStake(withdrawAddress);
