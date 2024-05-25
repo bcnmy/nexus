d35f1d4f011a47c6bcca0000f73f7bc0beb13054
aboudjem <boudjemaa.adam@gmail.com>
2024-05-25 15:22:47 +0400

    chore: Remove unused _mint function call in NFT.sol

--- contracts/mocks/NFT.sol
+++ contracts/mocks/NFT.sol
@@ -5,7 +5,6 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract NFT is ERC721 {
     constructor(string memory name, string memory symbol) ERC721(name, symbol) {
-        _mint(msg.sender, 10);
     }
 
     // Mint a new NFT token to the specified address with the specified tokenId
