b923ab2a9205eb8d247a7724df694ff1a021d3c3
aboudjem <boudjemaa.adam@gmail.com>
2024-05-27 16:55:09 +0400

    chore: Update lcov command in generate_coverage_report.sh

--- scripts/foundry/generate_coverage_report.sh
+++ scripts/foundry/generate_coverage_report.sh
@@ -9,17 +9,17 @@ forge coverage --ir-minimum --report lcov
 # Install lcov if not installed
 if ! command -v lcov &>/dev/null; then
   echo "lcov is not installed. Installing..."
-  sudo apt-get install lcov
+  sudo apt-get install -y lcov
 fi
 
 lcov --version
 
 # Exclude test, mock, and node_modules folders
 EXCLUDE="*test* *mocks* *node_modules* *scripts* *lib*"
-lcov --rc branch_coverage=1 --ignore-errors unused --ignore-errors inconsistent --remove lcov.info $EXCLUDE --output-file forge-pruned-lcov.info
+lcov --rc lcov_branch_coverage=1 --ignore-errors unused --ignore-errors inconsistent --remove lcov.info $EXCLUDE --output-file forge-pruned-lcov.info
 
 # Generate HTML report if not running in CI
 if [ "$CI" != "true" ]; then
-  genhtml forge-pruned-lcov.info --ignore-errors deprecated,inconsistent,corrupt --output-directory coverage/foundry
+  genhtml forge-pruned-lcov.info --branch-coverage --ignore-errors deprecated,inconsistent,corrupt --output-directory coverage/foundry
   open coverage/foundry/index.html
 fi
