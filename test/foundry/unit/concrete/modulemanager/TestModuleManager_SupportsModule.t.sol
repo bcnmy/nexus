a297a364598433146088bd1e3c86f4f75c70520b
aboudjem <boudjemaa.adam@gmail.com>
2024-05-27 17:19:58 +0400

    compiler warning: Update test_SupportsModule functions to be view functions

--- test/foundry/unit/concrete/modulemanager/TestModuleManager_SupportsModule.t.sol
+++ test/foundry/unit/concrete/modulemanager/TestModuleManager_SupportsModule.t.sol
@@ -11,32 +11,32 @@ contract TestModuleManager_SupportsModule is TestModuleManagement_Base {
     }
 
     /// @notice Tests the successful support of the Validator module.
-    function test_SupportsModuleValidator_Success() public {
+    function test_SupportsModuleValidator_Success() public view {
         assertTrue(BOB_ACCOUNT.supportsModule(MODULE_TYPE_VALIDATOR), "Validator module not supported");
     }
 
     /// @notice Tests the successful support of the Executor module.
-    function test_SupportsModuleExecutor_Success() public {
+    function test_SupportsModuleExecutor_Success() public view {
         assertTrue(BOB_ACCOUNT.supportsModule(MODULE_TYPE_EXECUTOR), "Executor module not supported");
     }
 
     /// @notice Tests the successful support of the Fallback module.
-    function test_SupportsModuleFallback_Success() public {
+    function test_SupportsModuleFallback_Success() public view {
         assertTrue(BOB_ACCOUNT.supportsModule(MODULE_TYPE_FALLBACK), "Fallback module not supported");
     }
 
     /// @notice Tests the successful support of the Hook module.
-    function test_SupportsModuleHook_Success() public {
+    function test_SupportsModuleHook_Success() public view {
         assertTrue(BOB_ACCOUNT.supportsModule(MODULE_TYPE_HOOK), "Hook module not supported");
     }
 
     /// @notice Tests that an unsupported module type returns false.
-    function test_SupportsModule_FailsForUnsupportedModule() public {
+    function test_SupportsModule_FailsForUnsupportedModule() public view {
         assertFalse(BOB_ACCOUNT.supportsModule(INVALID_MODULE_TYPE), "Invalid module type should not be supported");
     }
 
     /// @notice Tests that zero as a module type returns false.
-    function test_SupportsModule_FailsForZeroModuleType() public {
+    function test_SupportsModule_FailsForZeroModuleType() public view {
         assertFalse(BOB_ACCOUNT.supportsModule(0), "Zero module type should not be supported");
     }
 }
