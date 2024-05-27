b14f0645e25846aa6474eae3350492ae3e2b48d7
aboudjem <boudjemaa.adam@gmail.com>
2024-05-27 19:00:56 +0400

    chore: Update generate_coverage_report.sh to check if coverage file is created and list generated files

--- scripts/foundry/generate_coverage_report.sh
+++ scripts/foundry/generate_coverage_report.sh
@@ -18,8 +18,20 @@ mkdir -p coverage/foundry
 EXCLUDE="*test* *mocks* *node_modules* *scripts* *lib*"
 lcov --rc lcov_branch_coverage=1 --ignore-errors unused --ignore-errors inconsistent --remove lcov.info $EXCLUDE --output-file coverage/foundry/forge-pruned-lcov.info
 
+# Check if the coverage file is created
+if [ -f coverage/foundry/forge-pruned-lcov.info ]; then
+  echo "Foundry coverage report generated successfully."
+else
+  echo "Failed to generate Foundry coverage report."
+  exit 1
+fi
+
 # Generate HTML report if not running in CI
 if [ "$CI" != "true" ]; then
   genhtml coverage/foundry/forge-pruned-lcov.info --branch-coverage --ignore-errors deprecated,inconsistent,corrupt --output-directory coverage/foundry
   open coverage/foundry/index.html
 fi
+
+# List the generated files for debugging purposes
+echo "Generated files in coverage directory:"
+ls -R coverage
