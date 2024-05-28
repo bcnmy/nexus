9f1cfa374bb8f32fdd2878ea27b3bace6add894d
aboudjem <boudjemaa.adam@gmail.com>
2024-05-29 01:07:37 +0400

    refactor: Update MockPaymaster to extend VerifyingPaymaster

--- test/foundry/integration/TestNexusERC20Token_Integration_WarmAccess.t.sol
+++ test/foundry/integration/TestNexusERC20Token_Integration_WarmAccess.t.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.24;
 import "../utils/Imports.sol";
 import "../utils/NexusTest_Base.t.sol";
 
-/// @title TestNexusERC20TokenIntegration
-/// @notice Tests Nexus smart account functionalities with ERC20Token token transfers
-contract TestNexusERC20TokenIntegration is NexusTest_Base {
+/// @title TestNexusERC20Token_Integration_WarmAccess
+/// @notice Tests Nexus smart account functionalities with ERC20 token transfers (Warm Access)
+contract TestNexusERC20Token_Integration_WarmAccess is NexusTest_Base {
     Vm.Wallet private user;
     MockToken private ERC20Token;
     MockPaymaster private paymaster;
@@ -14,14 +14,15 @@ contract TestNexusERC20TokenIntegration is NexusTest_Base {
     address payable private preComputedAddress;
     address private constant recipient = address(0x123);
 
-    /// @notice Modifier to check ERC20Token balance changes
+    /// @notice Modifier to check ERC20 token balance changes with warm access
     /// @param account The account to check the balance for
     /// @param expectedBalance The expected balance after the operation
-    modifier checkERC20TokenBalance(address account, uint256 expectedBalance) {
-        uint256 initialBalance = ERC20Token.balanceOf(account);
+    modifier checkERC20TokenBalanceWarm(address account, uint256 expectedBalance) {
+        ERC20Token.transfer(account, 1);
+        assertGt(ERC20Token.balanceOf(account), 0, "Account balance is zero (warm access)");
         _;
         uint256 finalBalance = ERC20Token.balanceOf(account);
-        assertEq(finalBalance, initialBalance + expectedBalance);
+        assertEq(finalBalance, expectedBalance + 1);
     }
 
     /// @notice Sets up the initial state for the tests
@@ -37,16 +38,18 @@ contract TestNexusERC20TokenIntegration is NexusTest_Base {
         ERC20Token.transfer(preComputedAddress, amount);
     }
 
-    /// @notice Tests gas consumption for a simple ERC20Token transfer
-    function test_Gas_ERC20Token_Simple_Transfer() public checkERC20TokenBalance(recipient, amount) {
-        uint256 initialGas = gasleft();
-        ERC20Token.transfer(recipient, amount);
-        uint256 gasUsed = initialGas - gasleft();
-        emit log_named_uint("ERC20Token::transfer::SimpleTransfer::Gas used for ERC20Token transfer", gasUsed);
+    /// @notice Tests gas consumption for a simple ERC20 token transfer with warm access
+    function test_Gas_ERC20Token_Simple_Transfer_Warm() public checkERC20TokenBalanceWarm(recipient, amount) {
+        measureAndLogGasEOA(
+            "ERC20::transfer::EOA::Simple::WarmAccess",
+            address(ERC20Token),
+            0,
+            abi.encodeWithSignature("transfer(address,uint256)", recipient, amount)
+        );
     }
 
-    /// @notice Tests sending ERC20Token from an already deployed Nexus smart account
-    function test_Gas_ERC20Token_DeployedNexus_Transfer() public checkERC20TokenBalance(recipient, amount) {
+    /// @notice Tests sending ERC20 tokens from an already deployed Nexus smart account with warm access
+    function test_Gas_ERC20Token_DeployedNexus_Transfer_Warm() public checkERC20TokenBalanceWarm(recipient, amount) {
         Nexus deployedNexus = deployNexus(user, 100 ether, address(VALIDATOR_MODULE));
         ERC20Token.transfer(address(deployedNexus), amount);
 
@@ -59,14 +62,11 @@ contract TestNexusERC20TokenIntegration is NexusTest_Base {
 
         PackedUserOperation[] memory userOps = buildPackedUserOperation(user, deployedNexus, EXECTYPE_DEFAULT, executions, address(VALIDATOR_MODULE));
 
-        uint256 initialGas = gasleft();
-        ENTRYPOINT.handleOps(userOps, payable(BUNDLER.addr));
-        uint256 gasUsed = initialGas - gasleft();
-        emit log_named_uint("ERC20Token::transfer::DeployedNexusTransfer::Gas used for sending ERC20Token from deployed Nexus", gasUsed);
+        measureAndLogGas("ERC20::transfer::Nexus::Deployed::WarmAccess", userOps);
     }
 
-    /// @notice Tests deploying Nexus and transferring ERC20Token tokens using a paymaster
-    function test_Gas_ERC20Token_DeployWithPaymaster_Transfer() public checkERC20TokenBalance(recipient, amount) {
+    /// @notice Tests deploying Nexus and transferring ERC20 tokens using a paymaster with warm access
+    function test_Gas_ERC20Token_DeployWithPaymaster_Transfer_Warm() public checkERC20TokenBalanceWarm(recipient, amount) {
         bytes memory initCode = buildInitCode(user.addr, address(VALIDATOR_MODULE));
 
         Execution[] memory executions = prepareSingleExecution(
@@ -88,19 +88,13 @@ contract TestNexusERC20TokenIntegration is NexusTest_Base {
             uint128(3e6) // postOp gas limit
         );
 
-        uint256 initialGas = gasleft();
-
         userOps[0].signature = signUserOp(user, userOps[0]);
-        ENTRYPOINT.handleOps(userOps, BUNDLER_ADDRESS);
-        uint256 gasUsed = initialGas - gasleft();
-        emit log_named_uint(
-            "ERC20Token::transfer::DeployWithPaymasterTransfer::Gas used for deploying Nexus and sending ERC20Token with paymaster",
-            gasUsed
-        );
+
+        measureAndLogGas("ERC20::transfer::Setup And Call::WithPaymaster::WarmAccess", userOps);
     }
 
-    /// @notice Test deploying Nexus and transferring ERC20Token tokens using deposited funds without a paymaster
-    function test_Gas_ERC20Token_DeployUsingDeposit_Transfer() public checkERC20TokenBalance(recipient, amount) {
+    /// @notice Test deploying Nexus and transferring ERC20 tokens using deposited funds without a paymaster with warm access
+    function test_Gas_ERC20Token_DeployUsingDeposit_Transfer_Warm() public checkERC20TokenBalanceWarm(recipient, amount) {
         uint256 depositAmount = 1 ether;
 
         // Add deposit to the precomputed address
@@ -112,7 +106,7 @@ contract TestNexusERC20TokenIntegration is NexusTest_Base {
         // Create initCode for deploying the Nexus account
         bytes memory initCode = buildInitCode(user.addr, address(VALIDATOR_MODULE));
 
-        // Prepare execution to transfer ERC20Token tokens
+        // Prepare execution to transfer ERC20 tokens
         Execution[] memory executions = prepareSingleExecution(
             address(ERC20Token),
             0,
@@ -128,17 +122,39 @@ contract TestNexusERC20TokenIntegration is NexusTest_Base {
             address(VALIDATOR_MODULE)
         );
         userOps[0].initCode = initCode;
-
-        uint256 initialGas = gasleft();
-
         // Sign the user operation
         userOps[0].signature = signUserOp(user, userOps[0]);
-        ENTRYPOINT.handleOps(userOps, BUNDLER_ADDRESS);
+        measureAndLogGas("ERC20::transfer::Setup And Call::UsingDeposit::WarmAccess", userOps);
+    }
 
-        uint256 gasUsed = initialGas - gasleft();
-        emit log_named_uint(
-            "ERC20Token::transfer::DeployUsingDepositTransfer::Gas used for deploying Nexus and transferring ERC20Token using deposit",
-            gasUsed
+    /// @notice Test sending ETH to the Nexus account before deployment and then deploy with warm access
+    function test_Gas_DeployNexusWithPreFundedETH_Warm() public checkERC20TokenBalanceWarm(recipient, amount) {
+        // Send ETH directly to the precomputed address
+        vm.deal(preComputedAddress, 1 ether);
+        assertEq(address(preComputedAddress).balance, 1 ether, "ETH not sent to precomputed address");
+
+        // Create initCode for deploying the Nexus account
+        bytes memory initCode = buildInitCode(user.addr, address(VALIDATOR_MODULE));
+
+        // Prepare execution to transfer ERC20 tokens
+        Execution[] memory executions = prepareSingleExecution(
+            address(ERC20Token),
+            0,
+            abi.encodeWithSignature("transfer(address,uint256)", recipient, amount)
         );
+
+        // Build user operation with initCode and callData
+        PackedUserOperation[] memory userOps = buildPackedUserOperation(
+            user,
+            Nexus(preComputedAddress),
+            EXECTYPE_DEFAULT,
+            executions,
+            address(VALIDATOR_MODULE)
+        );
+        userOps[0].initCode = initCode;
+        // Sign the user operation
+        userOps[0].signature = signUserOp(user, userOps[0]);
+
+        measureAndLogGas("ERC20::transfer::Setup And Call::Using Pre-Funded Ether::WarmAccess", userOps);
     }
 }
