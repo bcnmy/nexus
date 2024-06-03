0a4ca29904199e60eae9fb8980d8f275efcbd386
aboudjem <boudjemaa.adam@gmail.com>
2024-06-04 01:21:09 +0400

    refactor: Add InnerCallFailed error to INexusEventsAndErrors.sol
    The commit adds the InnerCallFailed error to the INexusEventsAndErrors.sol interface. This error is thrown when an inner call fails. This addition enhances the error handling capabilities of the interface.
    
    Refactor the INexusEventsAndErrors.sol interface to include the InnerCallFailed error.

--- contracts/interfaces/INexusEventsAndErrors.sol
+++ contracts/interfaces/INexusEventsAndErrors.sol
@@ -58,4 +58,8 @@ interface INexusEventsAndErrors {
 
     /// @notice Error thrown when the provided implementation address is not a contract.
     error ImplementationIsNotAContract();
+
+    /// @notice Error thrown when an inner call fails.
+    error InnerCallFailed();
+
 }
