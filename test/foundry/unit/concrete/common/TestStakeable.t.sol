ccea2927280089e2b6b99b6195a12be350637244
aboudjem <boudjemaa.adam@gmail.com>
2024-06-04 04:51:06 +0400

    fix conflicts

--- test/foundry/unit/concrete/common/TestStakeable.t.sol
+++ test/foundry/unit/concrete/common/TestStakeable.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.26;
 
 import "../../../utils/NexusTest_Base.t.sol";
 import { IEntryPoint, IStakeManager } from "account-abstraction/contracts/interfaces/IEntryPoint.sol";
