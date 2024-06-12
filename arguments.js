1c9bd9db4d1ef2e725b1096c73998349781a016c
livingrockrises <90545960+livingrockrises@users.noreply.github.com>
2024-06-13 01:04:43 +0400

    respond to pr comments

--- arguments.js
+++
@@ -1,7 +0,0 @@
-// Example constructor arguments for the contract to be verified
-module.exports = [
-    "0x66Ae45ad5BE4be08a70AD99e9cF41e6d6884B06F",
-    "0x2cf491602ad22944D9047282aBC00D3e52F56B37",
-    "0x9C08e1CE188C29bAaeBc64A08cF2Ec44207749B6",
-    "0x1ad17f0Dc85Da8Ed2CBA7415D290cCb0D79355C9"
-]
