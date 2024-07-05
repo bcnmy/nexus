ae22531d90ac5714cae13a04429e76e7500ee2f6
Filipp Makarov <fmc@Filipps-MacBook-Pro.local>
2024-07-05 12:32:40 +0300

    fix lint

--- contracts/lib/local/LocalCallDataParserLib.sol
+++ contracts/lib/local/LocalCallDataParserLib.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 
-import { MODE_MODULE_ENABLE } from "contracts/types/Constants.sol";
-
 library LocalCallDataParserLib {
     function parseEnableModeData(bytes calldata packedData) 
         internal 
