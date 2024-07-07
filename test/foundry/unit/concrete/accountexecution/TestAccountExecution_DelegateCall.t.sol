f0593e2adf3bc3883fe4e32669d4f15bbaa50507
livingrockrises <90545960+livingrockrises@users.noreply.github.com>
2024-07-07 22:04:06 +0530

    add test for try execute delegate call

--- test/foundry/unit/concrete/accountexecution/TestAccountExecution_DelegateCall.t.sol
+++ test/foundry/unit/concrete/accountexecution/TestAccountExecution_DelegateCall.t.sol
@@ -34,7 +34,7 @@ contract TestAccountExecution_TryExecuteSingle is TestAccountExecution_Base {
 
 
         // Build UserOperation for single execution
-        PackedUserOperation[] memory userOps = buildPackedUserOperation(BOB, BOB_ACCOUNT, EXECTYPE_TRY, execution, address(VALIDATOR_MODULE));
+        PackedUserOperation[] memory userOps = buildPackedUserOperation(BOB, BOB_ACCOUNT, EXECTYPE_DEFAULT, execution, address(VALIDATOR_MODULE));
 
          bytes memory userOpCalldata = abi.encodeCall(
             Nexus.execute,
@@ -55,5 +55,49 @@ contract TestAccountExecution_TryExecuteSingle is TestAccountExecution_Base {
         ENTRYPOINT.handleOps(userOps, payable(address(BOB.addr)));
          // Assert that the value was set ie that execution was successful
         assertTrue(valueTarget.balance == value);
+    }
+
+    /// @notice Tests successful execution of a single operation.
+    function test_TryExecuteDelegateCall_Success() public {
+
+        (bool res, ) = payable(address(BOB_ACCOUNT)).call{ value: 2 ether}(""); // Fund BOB_ACCOUNT
+        assertEq(res, true, "Funding BOB_ACCOUNT should succeed");
+
+        // Initial state assertion
+        assertEq(counter.getNumber(), 0, "Counter should start at 0");
+        // Create calldata for the account to execute
+        address valueTarget = makeAddr("valueTarget");
+        uint256 value = 1 ether;
+
+        bytes memory sendValue =
+            abi.encodeWithSelector(MockDelegateTarget.sendValue.selector, valueTarget, value);
+
+        // placeholder
+        Execution[] memory execution = new Execution[](1);
+        execution[0] = Execution(address(counter), 0, abi.encodeWithSelector(Counter.incrementNumber.selector));
+
+
+        // Build UserOperation for single execution
+        PackedUserOperation[] memory userOps = buildPackedUserOperation(BOB, BOB_ACCOUNT, EXECTYPE_TRY, execution, address(VALIDATOR_MODULE));
+
+         bytes memory userOpCalldata = abi.encodeCall(
+            Nexus.execute,
+            (
+                ModeLib.encode(
+                    CALLTYPE_DELEGATECALL, EXECTYPE_TRY, MODE_DEFAULT, ModePayload.wrap(0x00)
+                ),
+                abi.encodePacked(address(delegateTarget), sendValue)
+            )
+        );
+
+        userOps[0].callData = userOpCalldata;
+
+        // Sign the operation
+        bytes32 userOpHash = ENTRYPOINT.getUserOpHash(userOps[0]);
+        userOps[0].signature = signMessage(BOB, userOpHash);
+
+        ENTRYPOINT.handleOps(userOps, payable(address(BOB.addr)));
+         // Assert that the value was set ie that execution was successful
+        assertTrue(valueTarget.balance == (value));
     }
 }
