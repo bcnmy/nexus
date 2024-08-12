96d926150df1b79a95ccb9c0fa24addf5cb1886b
aboudjem <boudjemaa.adam@gmail.com>
2024-08-12 21:34:59 +0200

    refactor: Improve withdrawDepositTo function in BaseAccount.sol

--- contracts/base/BaseAccount.sol
+++ contracts/base/BaseAccount.sol
@@ -74,24 +74,23 @@ contract BaseAccount is IBaseAccount {
             } // For gas estimation.
         }
     }
-/// @notice Withdraws ETH from the EntryPoint to a specified address.
-/// @param to The address to receive the withdrawn funds.
-/// @param amount The amount to withdraw.
-function withdrawDepositTo(address to, uint256 amount) external payable virtual onlyEntryPointOrSelf {
-    address entryPointAddress = _ENTRYPOINT;
-    /// @solidity memory-safe-assembly
-    assembly {
-        let freeMemPtr := mload(0x40) // Store the free memory pointer.
-        mstore(0x14, to) // Store the `to` argument.
-        mstore(0x34, amount) // Store the `amount` argument.
-        mstore(0x00, 0x205c2878000000000000000000000000) // `withdrawTo(address,uint256)`.
-        if iszero(call(gas(), entryPointAddress, 0, 0x10, 0x44, codesize(), 0x00)) {
-            returndatacopy(freeMemPtr, 0x00, returndatasize())
-            revert(freeMemPtr, returndatasize())
+
+    /// @notice Withdraws ETH from the EntryPoint to a specified address.
+    /// @param to The address to receive the withdrawn funds.
+    /// @param amount The amount to withdraw.
+    function withdrawDepositTo(address to, uint256 amount) external payable virtual onlyEntryPointOrSelf {
+        address entryPointAddress = _ENTRYPOINT;
+        /// @solidity memory-safe-assembly
+        assembly {
+            mstore(0x14, to) // Store the `to` argument directly at memory location 0x14.
+            mstore(0x34, amount) // Store the `amount` argument at memory location 0x34.
+            mstore(0x00, 0x205c2878000000000000000000000000) // Store the function selector at memory location 0x00.
+            if iszero(call(gas(), entryPointAddress, 0, 0x10, 0x44, codesize(), 0x00)) {
+                returndatacopy(0x0, 0x00, returndatasize()) // Copy return data to 0x0.
+                revert(0x0, returndatasize()) // Revert with data at 0x0.
+            }
         }
     }
-}
-
 
     /// @notice Gets the nonce for a particular key.
     /// @param key The nonce key.
