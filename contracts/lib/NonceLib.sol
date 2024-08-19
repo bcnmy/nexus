0d318e40e1676fecd1b26c35e5b47c6b24e198d7
Filipp Makarov <filmakarov@Filipps-MacBook-Pro-2.local>
2024-08-19 12:26:45 +0200

    gas opt

--- contracts/lib/NonceLib.sol
+++ contracts/lib/NonceLib.sol
@@ -24,7 +24,7 @@ library NonceLib {
     /// @return res boolean result, true if it is the Module Enable Mode
     function isModuleEnableMode(uint256 nonce) internal pure returns (bool res) {
         assembly {
-            let vmode := shr(248, shl(24, nonce))
+            let vmode := byte(3, nonce)
             res := eq(shl(248, vmode), MODE_MODULE_ENABLE)
         }
     }
