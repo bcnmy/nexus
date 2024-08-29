0abeca00848cbd12e5be7aa955638547de93998f
joepegler <joe@enso.finance>
2024-08-29 13:15:30 +0100

    feat/dynamic-deploy (#146)
    * feat/dynamic-deploy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,32 +3,34 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-storage-layout";
 import "hardhat-deploy";
+import { dynamicNetworkConfig } from "./scripts/hardhat/dynamicNetworkConfig";
 
 dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: {
-    version: "0.8.26",
-    settings: {
-      viaIR: true,
-      optimizer: {
-        enabled: true,
-        runs: 1000000,
-        details: {
-          yul: true,
-        },
-      },
-    },
-  },
-  networks: {
-    hardhat: {
-      allowUnlimitedContractSize: true
-    },
-  },
-  docgen: {
-    projectName: "Nexus",
-    projectDescription: "Nexus - Biconomy Modular Smart Account - ERC-7579",
-  },
+	solidity: {
+		version: "0.8.26",
+		settings: {
+			viaIR: true,
+			optimizer: {
+				enabled: true,
+				runs: 1000000,
+				details: {
+					yul: true,
+				},
+			},
+		},
+	},
+	networks: {
+		hardhat: {
+			allowUnlimitedContractSize: true,
+		},
+		...dynamicNetworkConfig(),
+	},
+	docgen: {
+		projectName: "Nexus",
+		projectDescription: "Nexus - Biconomy Modular Smart Account - ERC-7579",
+	},
 };
 
 export default config;
