2e83bed9f50670b9278aada12de5a597e70f7d7b
Filipp Makarov <fmc@Filipps-MacBook-Pro.local>
2024-10-30 18:35:56 +0300

    fix hardhat

--- test/hardhat/utils/operationHelpers.ts
+++ test/hardhat/utils/operationHelpers.ts
@@ -473,22 +473,18 @@ export async function getAccountDomainStructFields(
     await account.eip712Domain();
   return ethers.AbiCoder.defaultAbiCoder().encode(
     [
-      "bytes1",
       "bytes32",
       "bytes32",
       "uint256",
       "address",
       "bytes32",
-      "bytes32",
     ],
     [
-      fields, // matches Solidity
       ethers.keccak256(ethers.toUtf8Bytes(name)), // matches Solidity
       ethers.keccak256(ethers.toUtf8Bytes(version)), // matches Solidity
       chainId,
       verifyingContract,
-      salt,
-      ethers.keccak256(ethers.solidityPacked(["uint256[]"], [extensions])),
+      salt
     ],
   );
 }
