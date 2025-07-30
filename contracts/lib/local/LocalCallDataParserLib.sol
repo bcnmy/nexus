40647163a065268bcfb34430711e7923395440b7
Filipp Makarov <fmc@Filipps-MacBook-Pro.local>
2025-07-30 12:11:42 +0300

    chore/sol version gte instead of eq

--- contracts/lib/local/LocalCallDataParserLib.sol
+++ contracts/lib/local/LocalCallDataParserLib.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.27;
+pragma solidity ^0.8.27;
 
 library LocalCallDataParserLib {
     /// @dev Parses the `userOp.signature` to extract the module type, module initialization data,
